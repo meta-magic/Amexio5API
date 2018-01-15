d231712bc9dc8c1bc7283b6318daec2ed2b0794b
sagarjdhv968 <sagar.jadhav@metamagic.in>
2018-01-15 18:03:38 +0530

    Duplicate Module removed

--- src/app/layout/row/row.demo.module.ts
+++ src/app/layout/row/row.demo.module.ts
@@ -1,6 +1,6 @@
 import {NgModule} from "@angular/core";
 import {RouterModule} from "@angular/router";
-import {AmexioFormsModule, AmexioLayoutModule, AmexioNavModule, AmexioWidgetModule} from "amexio-ng-extensions";
+import { AmexioWidgetModule} from "amexio-ng-extensions";
 import {HttpModule} from "@angular/http";
 import {FormsModule} from "@angular/forms";
 import {CommonModule} from "@angular/common";
@@ -11,7 +11,7 @@ import {RowDemo} from "./row.demo";
  */
 @NgModule({
   declarations: [RowDemo],
-  imports: [CommonModule, FormsModule, HttpModule,SharedModule, AmexioFormsModule,AmexioLayoutModule, AmexioLayoutModule, AmexioNavModule, AmexioWidgetModule, RouterModule.forChild([{
+  imports: [CommonModule, FormsModule, HttpModule,SharedModule, AmexioWidgetModule, RouterModule.forChild([{
     path: '', component: RowDemo, pathMatch: 'full'
   }])],
   providers: []
