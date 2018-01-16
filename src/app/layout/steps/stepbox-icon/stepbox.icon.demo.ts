602476048016a7d2554922885c9f4023bd93beae
Pratik Kelwalkar <pratik.kelwalkar@metamagic.in>
2018-01-16 12:40:54 +0530

    Menus added

---
+++ src/app/layout/steps/stepbox-icon/stepbox.icon.demo.ts
@@ -0,0 +1,15 @@
+/**
+ * Created by pratik on 15/1/18.
+ */
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+ selector: 'stepbox-icon-demo',
+ templateUrl: 'S.component.html'
+})
+
+export class StepBoxIconComponent implements OnInit {
+ constructor() { }
+
+ ngOnInit() { }
+}
