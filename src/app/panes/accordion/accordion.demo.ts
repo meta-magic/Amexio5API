22061c18fcaa488f28b8d679e6c8f14e73ec6467
Pratik Kelwalkar <pratik.kelwalkar@metamagic.in>
2018-01-16 17:25:56 +0530

    Added Acccordion Content

--- src/app/panes/accordion/accordion.demo.ts
+++ src/app/panes/accordion/accordion.demo.ts
@@ -2,18 +2,128 @@
  * Created by pratik on 16/1/18.
  */
 import {Component, OnInit} from '@angular/core';
+import {Http} from "@angular/http";
 
 @Component({
   selector: 'accordion-demo',
   template: `
-    <h3>Accordion Demo</h3>
+    <amexio-card enableHeader="true">
+      <amexio-header>
+        <h2>Accordion Component</h2>
+      </amexio-header>
+      <amexio-body>
+        <p>Accordion provides an easy way to organize big forms by grouping the fields in accordion tabs</p>
+        <amexio-tab-view>
+          <amexio-tab title="Demo" active="true">
+            <amexio-accordion>
+              <amexio-accordion-tab header="Page One" expanded="true"> Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</amexio-accordion-tab>
+              <amexio-accordion-tab header="Page two"> Nullam nec dolor lobortis, dictum dolor ac, suscipit massa. Donec id suscipit nisi. Nunc sit amet aliquet risus. Aenean placerat suscipit risus at mollis. Quisque eleifend gravida scelerisque. In non eleifend nisi. Phasellus tempor hendrerit posuere. Praesent ornare rutrum mi et condimentum. Vestibulum tempus, urna non ultrices aliquam, ex ex blandit lacus, at porttitor odio ligula sed lacus. Praesent in luctus odio, at sodales orci. Vivamus vitae ullamcorper lectus. </amexio-accordion-tab>
+              <amexio-accordion-tab header="Page three"> Phasellus in condimentum elit. In et vestibulum sem. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Aliquam erat volutpat. Sed viverra libero vel massa accumsan aliquet. Mauris a dui nec sapien pretium euismod. Sed lobortis quis sapien a mollis. Nullam ex nibh, malesuada eget rutrum eu, dapibus quis ante. Proin non convallis augue, at tincidunt nisi. Cras et metus blandit, sollicitudin mi ac, fringilla libero.</amexio-accordion-tab>
+              <amexio-accordion-tab header="Page four">Etiam mattis fermentum arcu eu feugiat. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Aliquam porttitor, erat sit amet pharetra aliquam, ex leo tincidunt erat, in commodo leo magna quis mauris.</amexio-accordion-tab>
+            </amexio-accordion>
+          </amexio-tab>
+          <amexio-tab title="API Reference">
+            <amexio-datagrid title="Properties" [columnToggle]="false"
+                             [httpMethod]="'get'"
+                             [httpUrl]="'assets/apireference/layout/accordion/acc.json'"
+                             [dataReader]="'properties'"
+                             [filtering]="false" >
+              <amexio-data-table-column [dataIndex]="'name'" [dataType]="'string'" [hidden]="false"
+                                        [text]="'Name'"></amexio-data-table-column>
+              <amexio-data-table-column [dataIndex]="'type'" [dataType]="'string'" [hidden]="false"
+                                        [text]="'Type'"></amexio-data-table-column>
+              <amexio-data-table-column [dataIndex]="'default'" [dataType]="'string'" [hidden]="false"
+                                        [text]="'Default'"></amexio-data-table-column>
+              <amexio-data-table-column [dataIndex]="'description'" [dataType]="'string'" [hidden]="false"
+                                        [text]="'Description'"></amexio-data-table-column>
+            </amexio-datagrid>
+
+
+            <amexio-datagrid title="Events" [columnToggle]="false"
+                             [httpMethod]="'get'"
+                             [httpUrl]="'assets/apireference/layout/accordion/acc_event.json'"
+                             [dataReader]="'properties'"
+                             [filtering]="false" >
+              <amexio-data-table-column [dataIndex]="'name'" [dataType]="'string'" [hidden]="false"
+                                        [text]="'Name'"></amexio-data-table-column>
+              <amexio-data-table-column [dataIndex]="'type'" [dataType]="'string'" [hidden]="false"
+                                        [text]="'Type'"></amexio-data-table-column>
+              <amexio-data-table-column [dataIndex]="'default'" [dataType]="'string'" [hidden]="false"
+                                        [text]="'Default'"></amexio-data-table-column>
+              <amexio-data-table-column [dataIndex]="'description'" [dataType]="'string'" [hidden]="false"
+                                        [text]="'Description'"></amexio-data-table-column>
+            </amexio-datagrid>
+
+
+          </amexio-tab>
+          <amexio-tab title="Source">
+            <amexio-vertical-tab-view>
+              <amexio-tab title="HTML" [active]="true">
+                <ng-container *ngIf="htmlCode">
+                  <!--<clip-copy [htmlCode]="htmlCode" (onClick)="onCopyClick()"></clip-copy>-->
+                  <prism-block [code]="htmlCode" [language]="'html'"></prism-block>
+                </ng-container>
+              </amexio-tab>
+              <amexio-tab title="Type Script">
+                <ng-container *ngIf="typeScriptCode">
+                  <prism-block [code]="typeScriptCode" [language]="'typescript'"></prism-block>
+                </ng-container>
+              </amexio-tab>
+            </amexio-vertical-tab-view>
+          </amexio-tab>
+          <amexio-tab title="Live">
+            Work in Progress
+          </amexio-tab>
+        </amexio-tab-view>
+      </amexio-body>
+    </amexio-card>
   `
 })
 
-export class AccordionDemoComponent implements OnInit {
-  constructor() {
+export class AccordionDemoComponent{
+  htmlCode: string;
+  typeScriptCode: string;
+  copyMsgArray: any[];
+  asyncFlag : boolean;
+  constructor(private http: Http) {
+    this.getHtmlAndTypeScriptCode();
+  }
+  getDta() {
+    this.asyncFlag = true;
+    setTimeout(() => {
+      this.asyncFlag = false;
+    }, 3000);
+  }
+  //TO LOAD HTML AND TYPESCRIPT CODE
+  getHtmlAndTypeScriptCode() {
+    let responseHtml: any;
+    let responseTs: any;
+
+    //HTML FILE
+    this.http.get('assets/data/code/layout/accordion/accordion.html').subscribe(data => {
+      responseHtml = data.text();
+    }, error => {
+    }, () => {
+      this.htmlCode = responseHtml;
+    });
+
+    //TS FILE
+    this.http.get('assets/data/code/layout/accordion/accordion.text').subscribe(data => {
+      responseTs = data.text();
+    }, error => {
+    }, () => {
+      this.typeScriptCode = responseTs;
+    });
+
   }
 
-  ngOnInit() {
+  //THIS METHOD USED FOR COPY THE HTML & TYPESCRIPT CODE
+  onCopyClick() {
+    if (this.copyMsgArray.length >= 1) {
+      this.copyMsgArray = [];
+      this.copyMsgArray.push({'msg': 'Code Copied', 'type': 'info'});
+    } else {
+      this.copyMsgArray.push({'msg': 'Code Copied', 'type': 'info'});
+    }
   }
 }
