7ab8f76c196f5f50fa8d323555ed3c49d3e1eb10
sagarjdhv968 <sagar.jadhav@metamagic.in>
2018-01-17 16:42:25 +0530

    Menubar added

---
+++ src/app/navigation/menubar/menubar.demo.module.ts
@@ -0,0 +1,21 @@
+import {NgModule} from "@angular/core";
+import {RouterModule} from "@angular/router";
+import {AmexioWidgetModule} from "amexio-ng-extensions";
+import {HttpModule} from "@angular/http";
+import {FormsModule} from "@angular/forms";
+import {CommonModule} from "@angular/common";
+import {SharedModule} from "../../shared.module";
+import {MenuBarDemo} from "./menubar.demo";
+/**
+ * Created by sagar on 11/1/18.
+ */
+@NgModule({
+  declarations: [MenuBarDemo],
+  imports: [CommonModule, FormsModule, HttpModule,SharedModule, AmexioWidgetModule, RouterModule.forChild([{
+    path: '', component: MenuBarDemo, pathMatch: 'full'
+  }])],
+  providers: []
+})
+export class MenuBarDemoModule {
+
+}
