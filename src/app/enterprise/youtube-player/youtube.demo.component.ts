97569c0a38806311fae3aee841f3ab54b127f9bb
sagarjdhv968 <sagar.jadhav@metamagic.in>
2018-01-18 19:30:42 +0530

    Media Video

--- src/app/enterprise/youtube-player/youtube.demo.component.ts
+++ src/app/enterprise/youtube-player/youtube.demo.component.ts
@@ -2,16 +2,118 @@
  * Created by pratik on 18/1/18.
  */
 import { Component, OnInit } from '@angular/core';
+import {Http} from "@angular/http";
 
 @Component({
  selector: 'youtube-player',
  template: `
-   Youtube Player
+   <amexio-card header="true">
+     <amexio-header>
+       <h2>YouTube Video Player Component</h2>
+     </amexio-header>
+     <amexio-body>
+       <p>A simple configurable of video player.</p>
+       <amexio-tab-view>
+         <amexio-tab title="Demo" active="true">
+           <amexio-card enableheader="true">
+             <amexio-header><h2>Video player</h2></amexio-header>
+             <amexio-body>
+               <amexio-row>
+                 <amexio-column size="12" style="height: 550px">
+                   <amexio-ee-youtube-player url="https://www.youtube.com/embed/U7elNhHwgBU"
+                                             [height]="'450px'"
+                   >
+                     
+                   </amexio-ee-youtube-player>
+
+                 </amexio-column>
+               </amexio-row>
+             </amexio-body>
+           </amexio-card>
+         </amexio-tab>
+         <amexio-tab title="API Reference">
+           <amexio-datagrid title="Propeties" [enablecolumnfiter]="false"
+                            [httpmethod]="'get'"
+                            [httpurl]="'assets/apireference/enterprise/videoplayer.json'"
+                            [datareader]="'properties'"
+                            [enabledatafilter]="false">
+             <amexio-data-table-column [width]="15" [dataindex]="'name'" [datatype]="'string'" [hidden]="false"
+                                       [text]="'Name'"></amexio-data-table-column>
+             <amexio-data-table-column [width]="10" [dataindex]="'type'" [datatype]="'string'" [hidden]="false"
+                                       [text]="'Type'"></amexio-data-table-column>
+             <amexio-data-table-column [width]="10" [dataindex]="'default'" [datatype]="'string'" [hidden]="false"
+                                       [text]="'Default'"></amexio-data-table-column>
+             <amexio-data-table-column [width]="65" [dataindex]="'description'" [datatype]="'string'" [hidden]="false"
+                                       [text]="'Description'"></amexio-data-table-column>
+           </amexio-datagrid>
+
+         </amexio-tab>
+         <amexio-tab title="Source">
+           <amexio-vertical-tab-view>
+             <amexio-tab title="HTML" [active]="true">
+               <ng-container *ngIf="htmlCode">
+                 <!--<clip-copy [htmlCode]="htmlCode" (onClick)="onCopyClick()"></clip-copy>-->
+                 <prism-block [code]="htmlCode" [language]="'html'"></prism-block>
+               </ng-container>
+             </amexio-tab>
+             <amexio-tab title="Type Script">
+               <ng-container *ngIf="typeScriptCode">
+                 <prism-block [code]="typeScriptCode" [language]="'typescript'"></prism-block>
+               </ng-container>
+             </amexio-tab>
+           </amexio-vertical-tab-view>
+         </amexio-tab>
+         <amexio-tab title="Live">
+           Work in Progress
+         </amexio-tab>
+       </amexio-tab-view>
+     </amexio-body>
+   </amexio-card>
  `
 })
 
 export class YoutubePlayerDemoComponent implements OnInit {
- constructor() { }
+  htmlCode: string;
+  typeScriptCode: string;
+  copyMsgArray: any[];
+
+  constructor(private http: Http) {
+    this.getHtmlAndTypeScriptCode();
+  }
+
+  //TO LOAD HTML AND TYPESCRIPT CODE
+  getHtmlAndTypeScriptCode() {
+    let responseHtml: any;
+    let responseTs: any;
+
+    //HTML FILE
+    this.http.get('assets/data/code/enterprise/videoplayer/ee.html').subscribe(data => {
+      responseHtml = data.text();
+    }, error => {
+    }, () => {
+      this.htmlCode = responseHtml;
+    });
+
+    //TS FILE
+    this.http.get('assets/data/code/enterprise/videoplayer/ee.text').subscribe(data => {
+      responseTs = data.text();
+    }, error => {
+    }, () => {
+      this.typeScriptCode = responseTs;
+    });
+
+  }
+
+  //THIS METHOD USED FOR COPY THE HTML & TYPESCRIPT CODE
+  onCopyClick() {
+    if (this.copyMsgArray.length >= 1) {
+      this.copyMsgArray = [];
+      this.copyMsgArray.push({'msg': 'Code Copied', 'type': 'info'});
+    } else {
+      this.copyMsgArray.push({'msg': 'Code Copied', 'type': 'info'});
+    }
+  }
+
 
  ngOnInit() { }
 }
--- src/app/enterprise/youtube-player/youtube.demo.module.ts
+++ src/app/enterprise/youtube-player/youtube.demo.module.ts
@@ -5,13 +5,17 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import {YoutubePlayerDemoComponent} from "./youtube.demo.component";
+import {SharedModule} from "../../shared.module";
+import {CommonModule} from "@angular/common";
+import {HttpModule} from "@angular/http";
+import {AmexioEnterpriseEEModule, AmexioWidgetModule} from "amexio-ng-extensions";
 
 const routes: Routes = [
   { path: '', component: YoutubePlayerDemoComponent },
 ];
 
 @NgModule({
-  imports: [RouterModule.forChild(routes)],
+  imports: [SharedModule,CommonModule,HttpModule,AmexioWidgetModule,AmexioEnterpriseEEModule,RouterModule.forChild(routes)],
   exports: [RouterModule],
   declarations : [YoutubePlayerDemoComponent]
 })
