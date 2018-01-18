4a1c5f469e1ccc51c8445a580207adcb1861bffd
sagarjdhv968 <sagar.jadhav@metamagic.in>
2018-01-18 19:16:53 +0530

    Camel case changes

--- src/app/enterprise/content/content.compnent.ts
+++ src/app/enterprise/content/content.compnent.ts
@@ -1,17 +1,127 @@
 /**
  * Created by pratik on 18/1/18.
  */
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Http} from "@angular/http";
 
 @Component({
- selector: 'ee-content-demo',
- template : `
-  <h4>Content Demo module</h4>
- `
+  selector: 'ee-content-demo', template: `
+    <amexio-card header="true">
+      <amexio-header>
+        <h2>Media Content Component</h2>
+      </amexio-header>
+      <amexio-body>
+        <p>A simple configurable star rating component with visual feedback.</p>
+        <amexio-tab-view>
+          <amexio-tab title="Demo" active="true">
+            <amexio-card enableheader="true">
+              <amexio-header><h2>Media Content Demo</h2></amexio-header>
+              <amexio-body>
+                <amexio-ee-content
+                  [bgImgUrl]="'assets/images/narcos_main_header_img.jpg'"
+                  [title]="'Narcos'"
+                  [description]="'The true story of Colombia infamously violent and powerful drug cartels fuels this gritty gangster drama series.'"
+                  [contents]="content" [videoLink]="'https://www.youtube.com/embed/U7elNhHwgBU'" [matchPercentage]="'90%'"
+                  [ageLimit]="'16'" [releaseYear]="'2016'" [seasonNo]="1"
+                  [rate]="7" [max]="10" [enableWatch]="true" [enableMyList]="true">
+                </amexio-ee-content>
+              </amexio-body>
+            </amexio-card>
+          </amexio-tab>
+          <amexio-tab title="API Reference">
+            <amexio-datagrid title="Propeties" [enablecolumnfiter]="false"
+                             [httpmethod]="'get'"
+                             [httpurl]="'assets/apireference/enterprise/content.json'"
+                             [datareader]="'properties'"
+                             [enabledatafilter]="false">
+              <amexio-data-table-column [width]="15" [dataindex]="'name'" [datatype]="'string'" [hidden]="false"
+                                        [text]="'Name'"></amexio-data-table-column>
+              <amexio-data-table-column [width]="10" [dataindex]="'type'" [datatype]="'string'" [hidden]="false"
+                                        [text]="'Type'"></amexio-data-table-column>
+              <amexio-data-table-column [width]="10" [dataindex]="'default'" [datatype]="'string'" [hidden]="false"
+                                        [text]="'Default'"></amexio-data-table-column>
+              <amexio-data-table-column [width]="65" [dataindex]="'description'" [datatype]="'string'" [hidden]="false"
+                                        [text]="'Description'"></amexio-data-table-column>
+            </amexio-datagrid>
+            <br>
+            <amexio-datagrid title="Events" [enablecolumnfiter]="false"
+                             [httpmethod]="'get'"
+                             [httpurl]="'assets/apireference/enterprise/content.json'"
+                             [datareader]="'events'"
+                             [enabledatafilter]="false">
+              <amexio-data-table-column [width]="15" [dataindex]="'name'" [datatype]="'string'" [hidden]="false"
+                                        [text]="'Name'"></amexio-data-table-column>
+              <amexio-data-table-column [width]="65" [dataindex]="'description'" [datatype]="'string'" [hidden]="false"
+                                        [text]="'Description'"></amexio-data-table-column>
+            </amexio-datagrid>
+
+          </amexio-tab>
+          <amexio-tab title="Source">
+            <amexio-vertical-tab-view>
+              <amexio-tab title="HTML" [active]="true">
+                <ng-container *ngIf="htmlCode">
+                  <!--<clip-copy [htmlCode]="htmlCode" (onClick)="onCopyClick()"></clip-copy>-->
+                  <prism-block [code]="htmlCode" [language]="'html'"></prism-block>
+                </ng-container>
+              </amexio-tab>
+              <amexio-tab title="Type Script">
+                <ng-container *ngIf="typeScriptCode">
+                  <prism-block [code]="typeScriptCode" [language]="'typescript'"></prism-block>
+                </ng-container>
+              </amexio-tab>
+            </amexio-vertical-tab-view>
+          </amexio-tab>
+          <amexio-tab title="Live">
+            Work in Progress
+          </amexio-tab>
+        </amexio-tab-view>
+      </amexio-body>
+    </amexio-card>
+  `
 })
 
 export class EEContentDemoComponent implements OnInit {
- constructor() { }
+  htmlCode: string;
+  typeScriptCode: string;
+  copyMsgArray: any[];
 
- ngOnInit() { }
+  constructor(private http: Http) {
+    this.getHtmlAndTypeScriptCode();
+  }
+
+  //TO LOAD HTML AND TYPESCRIPT CODE
+  getHtmlAndTypeScriptCode() {
+    let responseHtml: any;
+    let responseTs: any;
+
+    //HTML FILE
+    this.http.get('assets/data/code/enterprise/content/ee.html').subscribe(data => {
+      responseHtml = data.text();
+    }, error => {
+    }, () => {
+      this.htmlCode = responseHtml;
+    });
+
+    //TS FILE
+    this.http.get('assets/data/code/enterprise/content/ee.text').subscribe(data => {
+      responseTs = data.text();
+    }, error => {
+    }, () => {
+      this.typeScriptCode = responseTs;
+    });
+
+  }
+
+  //THIS METHOD USED FOR COPY THE HTML & TYPESCRIPT CODE
+  onCopyClick() {
+    if (this.copyMsgArray.length >= 1) {
+      this.copyMsgArray = [];
+      this.copyMsgArray.push({'msg': 'Code Copied', 'type': 'info'});
+    } else {
+      this.copyMsgArray.push({'msg': 'Code Copied', 'type': 'info'});
+    }
+  }
+
+  ngOnInit() {
+  }
 }
