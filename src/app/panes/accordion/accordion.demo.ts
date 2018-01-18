f428b4d813f4a997d74cb23e667473d988440c2e
Pratik Kelwalkar <pratik.kelwalkar@metamagic.in>
2018-01-18 11:24:29 +0530

    Fixed Demo

--- src/app/panes/accordion/accordion.demo.ts
+++ src/app/panes/accordion/accordion.demo.ts
@@ -46,10 +46,6 @@ import {Http} from "@angular/http";
                              [enabledatafilter]="false" >
               <amexio-data-table-column [width]="15" [dataindex]="'name'" [datatype]="'string'" [hidden]="false"
                                         [text]="'Name'"></amexio-data-table-column>
-              <amexio-data-table-column [width]="10" [dataindex]="'type'" [datatype]="'string'" [hidden]="false"
-                                        [text]="'Type'"></amexio-data-table-column>
-              <amexio-data-table-column [width]="10" [dataindex]="'default'" [datatype]="'string'" [hidden]="false"
-                                        [text]="'Default'"></amexio-data-table-column>
               <amexio-data-table-column [width]="65" [dataindex]="'description'" [datatype]="'string'" [hidden]="false"
                                         [text]="'Description'"></amexio-data-table-column>
             </amexio-datagrid>
