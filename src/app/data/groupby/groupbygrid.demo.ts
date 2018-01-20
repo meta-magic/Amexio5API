0f229376fb98597804ffd95d605f9195e85aaffd
Pratik Kelwalkar <pratik.kelwalkar@metamagic.in>
2018-01-20 19:32:31 +0530

    Added Scroll

--- src/app/data/groupby/groupbygrid.demo.ts
+++ src/app/data/groupby/groupbygrid.demo.ts
@@ -81,24 +81,27 @@ import {Http} from "@angular/http";
 
           </amexio-tab>
           <amexio-tab title="Source">
-            <amexio-vertical-tab-view>
-              <amexio-tab title="HTML" [active]="true">
-                <ng-container *ngIf="htmlCode">
-                  <!--<clip-copy [htmlCode]="htmlCode" (onClick)="onCopyClick()"></clip-copy>-->
-                  <prism-block [code]="htmlCode" [language]="'html'"></prism-block>
-                </ng-container>
-              </amexio-tab>
-              <amexio-tab title="Type Script">
-                <ng-container *ngIf="typeScriptCode">
-                  <prism-block [code]="typeScriptCode" [language]="'typescript'"></prism-block>
-                </ng-container>
-              </amexio-tab>
-              <amexio-tab title="DataSource">
-                <ng-container *ngIf="dataSource">
-                  <prism-block [code]="dataSource" [language]="'json'"></prism-block>
-                </ng-container>
-              </amexio-tab>
-            </amexio-vertical-tab-view>
+            <div style="overflow-y: scroll;">
+              <amexio-vertical-tab-view>
+                <amexio-tab title="HTML" [active]="true">
+                  <ng-container *ngIf="htmlCode">
+                    <!--<clip-copy [htmlCode]="htmlCode" (onClick)="onCopyClick()"></clip-copy>-->
+                    <prism-block [code]="htmlCode" [language]="'html'"></prism-block>
+                  </ng-container>
+                </amexio-tab>
+                <amexio-tab title="Type Script">
+                  <ng-container *ngIf="typeScriptCode">
+                    <prism-block [code]="typeScriptCode" [language]="'typescript'"></prism-block>
+                  </ng-container>
+                </amexio-tab>
+                <amexio-tab title="DataSource">
+                  <ng-container *ngIf="dataSource">
+                    <prism-block [code]="dataSource" [language]="'json'"></prism-block>
+                  </ng-container>
+                </amexio-tab>
+              </amexio-vertical-tab-view>
+            </div>
+           
           </amexio-tab>
           <amexio-tab title="Live">
             Work in Progress
