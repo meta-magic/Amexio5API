3bc4d6e57a4cbbd60a3a8d19c4b1ffd9e49a1e2c
Pratik Kelwalkar <pratik.kelwalkar@metamagic.in>
2018-01-31 16:57:45 +0530

    getting-started : updated

--- src/app/gettingstarted/gettingstarted.ts
+++ src/app/gettingstarted/gettingstarted.ts
@@ -70,7 +70,7 @@ import {SharedModule} from "../shared.module";
               <amexio-row>
                 <amexio-column size="12">
                   <amexio-accordion>
-                    <amexio-accordion-tab header="Amexio Thanjavur v4.0" expanded="true">
+                    <amexio-accordion-tab header="Amexio Thanjavur v4.0" active="true">
                       <amexio-tab-view>
                         <amexio-tab title="Installation" active="true">
                           <amexio-row>
@@ -86,9 +86,12 @@ import {SharedModule} from "../shared.module";
                           <br>
                           <amexio-row>
                             <amexio-column size="12">
-                              <ng-container *ngIf="tsCode">
-                                <prism-block [code]="tsCode" [language]="'typescript'"></prism-block>
-                              </ng-container>
+                              <div style="height: 400px">
+                                <ng-container *ngIf="tsCode">
+                                  <prism-block [code]="tsCode" [language]="'typescript'"></prism-block>
+                                </ng-container>
+                              </div>
+                              
                             </amexio-column>
                           </amexio-row>
                         </amexio-tab>
@@ -112,76 +115,22 @@ import {SharedModule} from "../shared.module";
 
                         </amexio-tab>
                         <amexio-tab title="Services">
+                          <div style="height: 500px;overflow-x: auto;">
+                            
+                          
                           <p>Amexio Provides the below services :</p>
-                          <amexio-accordion>
-                            <amexio-accordion-tab header="Icon Loader Service">
+                          
+                            <h3>IconService</h3>
                               <span style="font-weight: bolder">Change all amexio icons from font-awesome to material and vice versa in a single line of code by injecting the <code>IconLoaderService</code></span>
                               <ng-container *ngIf="iconCode">
                                 <prism-block [code]="iconCode" [language]="'typescript'"></prism-block>
                               </ng-container>
-                            </amexio-accordion-tab>
-                            <amexio-accordion-tab header="CommonData Service">
+                            <br>
+                           <h3>CommonDataService</h3>
                               <span style="font-weight: bolder">By injecting <code>CommonDataService</code> into to module, you could easly fetch data from Rest</span>
                               <ng-container *ngIf="dataCode">
                                 <prism-block [code]="dataCode" [language]="'typescript'"></prism-block>
                               </ng-container>
-                            </amexio-accordion-tab>
-                          </amexio-accordion>
-                        </amexio-tab>
-                        <amexio-tab title="Themes">
-                          <div style="height: 500px;overflow-x : auto">
-                           <span>To use the default include the <pre><code>../node_module/amexio-ng-extensions/styles/mda/at-md-blue.scss</code></pre>in your app .angular-cli.json <code
-                             style="font-weight: bolder">styles</code> key</span>
-
-
-                            <ng-container *ngIf="styleCode">
-                              <div style="max-height: 300px;overflow-y: scroll;">
-                                <prism-block [code]="styleCode" [language]="'json'"></prism-block>
-                              </div>
-
-                            </ng-container>
-
-                            <p>Or refer the below table for other themes provided.</p>
-
-                            <amexio-row>
-                              <amexio-column size="12">
-                                <amexio-datagrid title="Amexio Material Themes" [enable-column-fiter]="false"
-                                                 [height]="300"
-                                                 [http-method]="'get'"
-                                                 [http-url]="'assets/apireference/modules/themes.json'"
-                                                 [data-reader]="'properties'"
-                                                 [enable-data-filter]="false">
-                                  <amexio-data-table-column [width]="15" [data-index]="'name'" [data-type]="'string'"
-                                                            [hidden]="false"
-                                                            [text]="'Theme Name'">
-                                  </amexio-data-table-column>
-                                  <amexio-data-table-column [width]="65" [data-index]="'description'"
-                                                            [data-type]="'string'"
-                                                            [hidden]="false"
-                                                            [text]="'Path'"></amexio-data-table-column>
-                                </amexio-datagrid>
-                              </amexio-column>
-                            </amexio-row>
-
-                            <amexio-row>
-                              <amexio-column size="12">
-                                <amexio-datagrid title="Amexio Themes" [enable-column-fiter]="false"
-                                                 [height]="300"
-                                                 [http-method]="'get'"
-                                                 [http-url]="'assets/apireference/modules/amx_themes.json'"
-                                                 [data-reader]="'properties'"
-                                                 [enable-data-filter]="false">
-                                  <amexio-data-table-column [width]="15" [data-index]="'name'" [data-type]="'string'"
-                                                            [hidden]="false"
-                                                            [text]="'Theme Name'">
-                                  </amexio-data-table-column>
-                                  <amexio-data-table-column [width]="65" [data-index]="'description'"
-                                                            [data-type]="'string'"
-                                                            [hidden]="false"
-                                                            [text]="'Path'"></amexio-data-table-column>
-                                </amexio-datagrid>
-                              </amexio-column>
-                            </amexio-row>
                           </div>
                         </amexio-tab>
                       </amexio-tab-view>
@@ -221,7 +170,7 @@ import {SharedModule} from "../shared.module";
                         <amexio-column size="12">
 
                           <b style="color: #323188">To install Amexio Hampi v2.0 ,</b><br>
-                          <pre style="font-weight: bold;color: #c0392b"><code>npm install amexio-ng-extensions@2.0.0 --save</code></pre>
+                          <pre style="font-weight: bold;color: #c0392b"><code>npm install amexio-ng-extensions@2.1.7 --save</code></pre>
 
                           Please make sure you have the latest version of
                           <pre style="font-weight: bold;color: #c0af1a"><code>NodeJS</code> & <code>npm</code> & <code>Angular CLI</code></pre>
@@ -269,59 +218,82 @@ import {SharedModule} from "../shared.module";
           </div>
         </amexio-column>
       </amexio-row>
+      
+      <amexio-row>
+        <amexio-column size="12">
+          <amexio-panel [header]="true"
+                        [title]="'Themes'"
+                        [expanded]="true">
+            <p style="color: darkred"> Themes are available from Version 4 </p>
+                           <span>To use the default include the <pre><code>../node_module/amexio-ng-extensions/styles/mda/at-md-blue.scss</code></pre>in your app .angular-cli.json <code
+                             style="font-weight: bolder">styles</code> key</span>
 
 
-      <br>
+            <ng-container *ngIf="styleCode">
+              <div style="max-height: 300px;overflow-y: scroll;">
+                <prism-block [code]="styleCode" [language]="'json'"></prism-block>
+              </div>
+
+            </ng-container>
+
+            <p>Or refer the below table for other themes provided.</p>
+
+            <amexio-row>
+              <amexio-column size="12">
+                <amexio-datagrid title="Amexio Material Themes" [enable-column-fiter]="false"
+                                 [height]="300"
+                                 [http-method]="'get'"
+                                 [http-url]="'assets/apireference/modules/themes.json'"
+                                 [data-reader]="'properties'"
+                                 [enable-data-filter]="false">
+                  <amexio-data-table-column [width]="15" [data-index]="'name'" [data-type]="'string'"
+                                            [hidden]="false"
+                                            [text]="'Theme Name'">
+                  </amexio-data-table-column>
+                  <amexio-data-table-column [width]="65" [data-index]="'description'"
+                                            [data-type]="'string'"
+                                            [hidden]="false"
+                                            [text]="'Path'"></amexio-data-table-column>
+                </amexio-datagrid>
+              </amexio-column>
+            </amexio-row>
+
+            <amexio-row>
+              <amexio-column size="12">
+                <amexio-datagrid title="Amexio Themes" [enable-column-fiter]="false"
+                                 [height]="300"
+                                 [http-method]="'get'"
+                                 [http-url]="'assets/apireference/modules/amx_themes.json'"
+                                 [data-reader]="'properties'"
+                                 [enable-data-filter]="false">
+                  <amexio-data-table-column [width]="15" [data-index]="'name'" [data-type]="'string'"
+                                            [hidden]="false"
+                                            [text]="'Theme Name'">
+                  </amexio-data-table-column>
+                  <amexio-data-table-column [width]="65" [data-index]="'description'"
+                                            [data-type]="'string'"
+                                            [hidden]="false"
+                                            [text]="'Path'"></amexio-data-table-column>
+                </amexio-datagrid>
+              </amexio-column>
+            </amexio-row>
+          </amexio-panel>
+        </amexio-column>
+      </amexio-row>
 
       <amexio-row>
         <amexio-column size="12">
-          <amexio-card header="true">
-            <amexio-header> License</amexio-header>
-            <amexio-body>
-              <amexio-card header="true">
-                <amexio-header>
-                  Apache License
-                </amexio-header>
-
-                <amexio-body>
-                  <p> Apache License
-                    Version 2.0, January 2004
-                    http://www.apache.org/licenses/</p>
-
-
-                  <p><b>TERMS AND CONDITIONS FOR USE, REPRODUCTION, AND DISTRIBUTION</b></p>
-
-                  <p>1. Definitions.</p>
-
-                  <p> "License" shall mean the terms and conditions for use, reproduction,
-                    and distribution as defined by Sections 1 through 9 of this document.</p>
-                  <p>
-                    "Licensor" shall mean the copyright owner or entity authorized by
-                    the copyright owner that is granting the License.
-                  </p>
-                  <p>
-                    "Legal Entity" shall mean the union of the acting entity and all
-                    other entities that control, are controlled by, or are under common
-                    control with that entity. For the purposes of this definition,
-                    "control" means (i) the power, direct or indirect, to cause the
-                    direction or management of such entity, whether by contract or
-                    otherwise, or (ii) ownership of fifty percent (50%) or more of the
-                    outstanding shares, or (iii) beneficial ownership of such entity.
-                  </p>
-                  <p>
-                    "You" (or "Your") shall mean an individual or Legal Entity
-                    exercising permissions granted by this License.
-                  </p>
-                  <p>
-                    "Source" form shall mean the preferred form for making modifications,
-                    including but not limited to software source code, documentation
-                    source, and configuration files.<a href="" style="font-weight: bold;color: #323188;">Continue Reading..</a>
-                  </p>
-                </amexio-body>
-
-              </amexio-card>
-            </amexio-body>
-          </amexio-card>
+          <amexio-panel [header]="true"
+                        [title]="'License'"
+                        [expanded]="true">
+            <br>
+            <b>Amexio API License for Standard & Enterprise Edition</b>
+            <p>
+              Amexio API is Free & Open Source & based on Apache 2 License. A Permissive License whose main condition require preservation of copyright and license notices.
+              Contributors provide an express grant of patent rights. Licensed works, modification and larger works may be distributed under different terms and without source code.
+            </p>
+            <p><a href="" style="color: blue">Click here to read the Apache 2 License. </a></p>
+          </amexio-panel>
         </amexio-column>
       </amexio-row>
 
