ebda392849335eb647ee50e59301fa82b38c5b7b
rashmithakkar <rashmi.thakkar@metamagic.in>
2018-02-05 18:52:31 +0530

    Stackblitz aaded to tab components

--- src/app/layout/tab/closabletab/closabletab.demo.ts
+++ src/app/layout/tab/closabletab/closabletab.demo.ts
@@ -98,8 +98,8 @@ import {Http} from "@angular/http";
             </div>
           </amexio-tab>
           <amexio-tab title="Live">
-            Work in Progress
-          </amexio-tab>
+          <p align="center">Amexio Sandbox</p>
+          <iframe style="width: 100%; height: 600px" src="https://stackblitz.com/edit/amexio-v4-horizontal-closable-tab" frameborder="0" allowfullscren="allowfullscren"></iframe>          </amexio-tab>
         </amexio-tab-view>
       </amexio-body>
     </amexio-card>
