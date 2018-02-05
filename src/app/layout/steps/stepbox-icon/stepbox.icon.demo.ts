21d5eecb1c5cab7e0704770d7899806ca9892b77
rashmithakkar <rashmi.thakkar@metamagic.in>
2018-02-05 18:37:09 +0530

    Stackblitz added to Panel except tab

--- src/app/layout/steps/stepbox-icon/stepbox.icon.demo.ts
+++ src/app/layout/steps/stepbox-icon/stepbox.icon.demo.ts
@@ -94,7 +94,8 @@ import {debug} from "util";
            </div>
          </amexio-tab>
          <amexio-tab title="Live">
-           Work in Progress
+         <p align="center">Amexio Sandbox</p>
+         <iframe style="width: 100%; height: 600px" src="https://stackblitz.com/edit/amexio-v4-stepboxicon" frameborder="0" allowfullscren="allowfullscren"></iframe>
          </amexio-tab>
        </amexio-tab-view>
      </amexio-body>
--- src/app/panes/carousel/carousel.demo.ts
+++ src/app/panes/carousel/carousel.demo.ts
@@ -89,8 +89,8 @@ import {Http} from "@angular/http";
           
          </amexio-tab>
          <amexio-tab title="Live">
-           Work in Progress
-         </amexio-tab>
+         <p align="center">Amexio Sandbox</p>
+         <iframe style="width: 100%; height: 600px" src="https://stackblitz.com/edit/amexio-v4-panel-carousel" frameborder="0" allowfullscren="allowfullscren"></iframe>         </amexio-tab>
        </amexio-tab-view>
      </amexio-body>
    </amexio-card>
