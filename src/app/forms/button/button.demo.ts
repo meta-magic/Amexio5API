8f98e60448779bab88f44810b36fad666fa7a7b2
ashwiniagre1 <ashwini.agre@metamagic.in>
2018-02-07 13:26:05 +0530

    updated stackblitz added

--- src/app/forms/button/button.demo.ts
+++ src/app/forms/button/button.demo.ts
@@ -177,7 +177,7 @@ import {Http} from "@angular/http";
           </amexio-tab>
           <amexio-tab title="Live">
           <p align="center">Amexio Sandbox</p>
-<iframe style="width: 100%; height: 600px" src="https://stackblitz.com/edit/amexio-v4-button" frameborder="0" allowfullscren="allowfullscren"></iframe>
+<iframe style="width: 100%; height: 600px" src="https://stackblitz.com/edit/amexio-v4-button?embed=1&file=app/forms/button/button.demo.html" frameborder="0" allowfullscren="allowfullscren"></iframe>
           </amexio-tab>
         </amexio-tab-view>
       </amexio-body>
--- src/app/forms/toggle/toggle.demo.ts
+++ src/app/forms/toggle/toggle.demo.ts
@@ -84,7 +84,7 @@ import {Http} from "@angular/http";
           </amexio-tab>
           <amexio-tab title="Live">
           <p align="center">Amexio Sandbox</p>
-<iframe style="width: 100%; height: 600px" src="https://stackblitz.com/edit/amexio-v4-toggle" frameborder="0" allowfullscren="allowfullscren"></iframe>
+<iframe style="width: 100%; height: 600px" src="https://stackblitz.com/edit/amexio-v4-toggle?embed=1&file=app/forms/toggle/toggle.demo.html" frameborder="0" allowfullscren="allowfullscren"></iframe>
           </amexio-tab>
         </amexio-tab-view>
       </amexio-body>
