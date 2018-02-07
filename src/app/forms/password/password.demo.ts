8ac865b30a9d2dc3cebbcd0bc11210138cebb480
ashwiniagre1 <ashwini.agre@metamagic.in>
2018-02-07 12:26:17 +0530

    updated stackblitz added

--- src/app/forms/password/password.demo.ts
+++ src/app/forms/password/password.demo.ts
@@ -103,7 +103,7 @@ import {Http} from "@angular/http";
           </amexio-tab>
           <amexio-tab title="Live">
           <p align="center">Amexio Sandbox</p>
-<iframe style="width: 100%; height: 600px" src="https://stackblitz.com/edit/amexio-v4-password-input" frameborder="0" allowfullscren="allowfullscren"></iframe>
+<iframe style="width: 100%; height: 600px" src="https://stackblitz.com/edit/amexio-v4-password-input?embed=1&file=app/forms/password/password.demo.html" frameborder="0" allowfullscren="allowfullscren"></iframe>
           </amexio-tab>
         </amexio-tab-view>
       </amexio-body>
