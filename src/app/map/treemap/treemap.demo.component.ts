a3c03cebb4fe21db6328810f801af69786ee5f3d
ashwiniagre1 <ashwini.agre@metamagic.in>
2018-02-07 14:53:07 +0530

    stackblitz url updated for map and dashboard

--- src/app/map/treemap/treemap.demo.component.ts
+++ src/app/map/treemap/treemap.demo.component.ts
@@ -77,7 +77,7 @@ import {Http} from "@angular/http";
           </amexio-tab>
           <amexio-tab title="Live">
           <p align="center">Amexio Sandbox</p>
-<iframe style="width: 100%; height: 600px" src="https://stackblitz.com/edit/amexi-v4-treemap" frameborder="0" allowfullscren="allowfullscren"></iframe>
+<iframe style="width: 100%; height: 600px" src="https://stackblitz.com/edit/amexi-v4-treemap?embed=1&file=app/map/treemap/treemap.demo.html" frameborder="0" allowfullscren="allowfullscren"></iframe>
           </amexio-tab>
         </amexio-tab-view>
       </amexio-body>
