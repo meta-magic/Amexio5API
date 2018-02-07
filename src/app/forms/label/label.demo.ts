6c4e722822280539e936f85feab84c1f80dfca4e
Pratik Kelwalkar <pratik.kelwalkar@metamagic.in>
2018-02-07 16:26:01 +0530

    label demo added

--- src/app/forms/label/label.demo.ts
+++ src/app/forms/label/label.demo.ts
@@ -12,34 +12,27 @@ import {Http} from "@angular/http";
         Amexio Label
       </amexio-header>
       <amexio-body>
-        <p>Number input component has been created with different configurable attributes for validation (min/max value, allow blank, custom regex),
-          custom error message, help, custom styles.</p>
+        <p>Amexio Label can be easily wrapped around any text and configure using the different responsive styling</p>
         <amexio-tab-view>
           <amexio-tab title="Demo" active="true">
             <amexio-row>
               <amexio-column size="6">
                 <amexio-card header="true">
                   <amexio-header>
-                    Basic Number Input
+                    Label size Large
                   </amexio-header>
                   <amexio-body>
-                    <amexio-number-input [field-label]="'Number Input'"
-                                         [place-holder]="'Enter Age'">
-                    </amexio-number-input>
+                    <amexio-label size="large">This is large amexio label</amexio-label>
                   </amexio-body>
                 </amexio-card>
               </amexio-column>
               <amexio-column [size]="6">
                 <amexio-card [header]="true">
                   <amexio-header>
-                    Number Input With Disable
+                    Label size Large - Bold
                   </amexio-header>
                   <amexio-body>
-                    <amexio-number-input
-                      [place-holder]="'Enter age'"
-                      [icon-feedback]="false"
-                      [has-label]="false" [disabled]="true">
-                    </amexio-number-input>
+                   <amexio-label size="large-bold">This is large-bold amexio label</amexio-label>
                   </amexio-body>
                 </amexio-card>
               </amexio-column>
@@ -48,31 +41,64 @@ import {Http} from "@angular/http";
               <amexio-column size="6">
                 <amexio-card [header]="true">
                   <amexio-header>
-                    Number Input Font Style
+                    Label size medium
                   </amexio-header>
                   <amexio-body>
-                    <amexio-number-input [field-label]="'Age'"
-                                         [place-holder]="'Enter Age'"
-                                         [font-style]="'italic'">
-                    </amexio-number-input>
+                    <amexio-label size="medium">This is medium amexio label</amexio-label>
                   </amexio-body>
                 </amexio-card>
               </amexio-column>
               <amexio-column [size]="6">
                 <amexio-card [header]="true">
                   <amexio-header>
-                    Number Input With Min/Max Validation
+                    Label with medium-bold size
                   </amexio-header>
                   <amexio-body>
-                    <amexio-number-input  [enable-popover]="true" [field-label]="'Age'"
-                                          [place-holder]="'Enter age'"
-                                          [allow-blank]="false" [error-msg] ="'Please enter age'"
-                                          [min-value]="1"
-                                          [min-error-msg]="'age can not be less than 1'"
-                                          [max-value]="100"  [max-error-msg]="'age can not be greater than 100'"
-                                          [icon-feedback]="true"
-                    >
-                    </amexio-number-input>
+                   <amexio-label size="medium-bold">This is medium-bold amexio label</amexio-label>
+                  </amexio-body>
+                </amexio-card>
+              </amexio-column>
+            </amexio-row>
+            <amexio-row>
+              <amexio-column size="6">
+                <amexio-card header="true">
+                  <amexio-header>
+                    Label size small
+                  </amexio-header>
+                  <amexio-body>
+                    <amexio-label>This is small amexio label</amexio-label>
+                  </amexio-body>
+                </amexio-card>
+              </amexio-column>
+              <amexio-column [size]="6">
+                <amexio-card [header]="true">
+                  <amexio-header>
+                    Label size small - Bold
+                  </amexio-header>
+                  <amexio-body>
+                    <amexio-label size="small-bold">This is small-bold amexio label</amexio-label>
+                  </amexio-body>
+                </amexio-card>
+              </amexio-column>
+            </amexio-row>
+            <amexio-row>
+              <amexio-column size="6">
+                <amexio-card header="true">
+                  <amexio-header>
+                    Colored Label size Medium Bold
+                  </amexio-header>
+                  <amexio-body>
+                    <amexio-label size="medium-bold" font-color="#FF5733">This is  amexio label</amexio-label>
+                  </amexio-body>
+                </amexio-card>
+              </amexio-column>
+              <amexio-column [size]="6">
+                <amexio-card [header]="true">
+                  <amexio-header>
+                    Colored Label size Small - Bold
+                  </amexio-header>
+                  <amexio-body>
+                    <amexio-label size="small-bold" font-color="#75FF33">This is small-bold amexio label</amexio-label>
                   </amexio-body>
                 </amexio-card>
               </amexio-column>
@@ -81,7 +107,7 @@ import {Http} from "@angular/http";
           <amexio-tab title="API Reference">
             <amexio-datagrid title="Properties" [enable-column-fiter]="false"
                              [http-method]="'get'"
-                             [http-url]="'assets/apireference/forms/number.json'"
+                             [http-url]="'assets/apireference/forms/label.json'"
                              [data-reader]="'properties'"
                              [enable-data-filter]="false" >
               <amexio-data-table-column [width]="15" [data-index]="'name'" [data-type]="'string'" [hidden]="false"
@@ -94,14 +120,6 @@ import {Http} from "@angular/http";
                                         [text]="'Description'"></amexio-data-table-column>
             </amexio-datagrid>
             <br>
-            <amexio-datagrid title="Events" [http-method]="'get'"
-                             [http-url]="'assets/apireference/forms/number.json'" [data-reader]="'events'"
-                             [enable-data-filter]="false">
-              <amexio-data-table-column [width]="20"[data-index]="'name'" [data-type]="'string'" [hidden]="false"
-                                        [text]="'Name'"></amexio-data-table-column>
-              <amexio-data-table-column [width]="80" [data-index]="'description'" [data-type]="'string'" [hidden]="false"
-                                        [text]="'Description'"></amexio-data-table-column>
-            </amexio-datagrid>
 
           </amexio-tab>
           <amexio-tab title="Source">
@@ -147,7 +165,7 @@ export class LabelDemo {
     let responseTs:any;
 
     //HTML FILE
-    this.http.get('assets/data/code/forms/number/form.html').subscribe(data => {
+    this.http.get('assets/data/code/forms/label/label.html').subscribe(data => {
       responseHtml = data.text();
     }, error => {
     }, () => {
@@ -155,7 +173,7 @@ export class LabelDemo {
     });
 
     //TS FILE
-    this.http.get('assets/data/code/forms/number/form.text').subscribe(data => {
+    this.http.get('assets/data/code/forms/label/label.text').subscribe(data => {
       responseTs = data.text();
     }, error => {
     }, () => {
