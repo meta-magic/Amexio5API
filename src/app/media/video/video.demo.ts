b8b0e3f18af22f50a199f01a620d537679696767
ashwiniagre1 <ashwini.agre@metamagic.in>
2018-02-08 12:34:57 +0530

    changes updated

--- src/app/media/video/video.demo.ts
+++ src/app/media/video/video.demo.ts
@@ -65,7 +65,7 @@ import {Http} from "@angular/http";
           </amexio-tab>
           <amexio-tab title="Live">
             <p align="center">Amexio Sandbox</p>
-<iframe style="width: 100%; height: 600px" src="https://stackblitz.com/edit/amexio-v4-video?embed=1&file=app/media/video/video.demo.html&view=preview" frameborder="0" allowfullscren="allowfullscren"></iframe>
+<iframe style="width: 100%; height: 600px" src="https://embed.plnkr.co/sM1HxXKKemniINKhc4Q0/" frameborder="0" allowfullscren="allowfullscren"></iframe>
           </amexio-tab>
         </amexio-tab-view>
       </amexio-body>
