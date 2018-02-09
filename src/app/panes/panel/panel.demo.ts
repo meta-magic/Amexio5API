4cc4e2eab437cc8f34df7b3720c941c9b919f919
Pratik Kelwalkar <pratik.kelwalkar@metamagic.in>
2018-02-09 15:12:16 +0530

    panel

--- src/app/panes/panel/panel.demo.ts
+++ src/app/panes/panel/panel.demo.ts
@@ -24,7 +24,8 @@ import {Http} from "@angular/http";
               <amexio-column [size]="4">
                 <amexio-panel [header]="true"  title="Custom Header" expanded="true">
                   <amexio-header>
-                        <i class="fa fa-refresh fa-fw" aria-hidden="true" (click)="refreshData()"></i>
+                    <amexio-image [icon-class]="'fa fa-refresh fa-fw'" (onClick)="refreshData()">
+                    </amexio-image>
                   </amexio-header>
                   Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
                 </amexio-panel>
