63f38a5ec539783b47f2ea30151ff074561367a8
ankita202 <ankita.jain@metamgic.in>
2018-05-16 10:30:15 +0530

    Tags:checkboxgroup,Layout:amexio-border-layout removed

--- src/app/forms/checkboxgroup/checkboxgroup.demo.ts
+++ src/app/forms/checkboxgroup/checkboxgroup.demo.ts
@@ -74,7 +74,7 @@ import {HttpClient} from "@angular/common/http";
 
           </amexio-tab>
           <amexio-tab title="API Reference">
-            <amexio-datagrid title="Properties <amexio-checkbox>" [enable-column-fiter]="false"
+            <amexio-datagrid title="Properties<amexio-checkbox>" [enable-column-fiter]="false"
                              [http-method]="'get'"
                              [http-url]="'assets/apireference/forms/checkbox.json'"
                              [data-reader]="'properties'"
