a3b379363111449e42962164b7bc4b6b73d3c3ec
kedar8 <kedar.kokil@metamagic.in>
2018-06-21 19:36:53 +0530

    dropdownmenu: changes updated

--- src/app/data/listbox/listbox.demo.ts
+++ src/app/data/listbox/listbox.demo.ts
@@ -172,7 +172,7 @@ import {HttpClient} from "@angular/common/http";
           </amexio-tab>
           <amexio-tab title="Live">
           <p align="center">Amexio Sandbox</p>
-          <iframe style="width: 100%; height: 600px" src="https://stackblitz.com/edit/amexio-v4-listbox?embed=1&file=app/data/listbox/listbox.demo.component.ts&view=editor" frameborder="0" allowfullscren="allowfullscren"></iframe>
+          <iframe style="width: 100%; height: 600px" src="https://stackblitz.com/edit/amexio-v4-listbox?embed=1&file=app/data/listbox/listbox.demo.component.ts" frameborder="0" allowfullscren="allowfullscren"></iframe>
           </amexio-tab>
         </amexio-tab-view>
       </amexio-body>
--- src/app/data/tree/simpletree/simpletree.demo.ts
+++ src/app/data/tree/simpletree/simpletree.demo.ts
@@ -91,7 +91,7 @@ import {HttpClient} from "@angular/common/http";
           <p align="center">Amexio Sandbox</p>
             
             
-          <iframe style="width: 100%; height: 600px" src="https://stackblitz.com/edit/amexio-v4-simple-tree?embed=1&file=app/tree/simpletree/simpletree.demo.html&view=editor" frameborder="0" allowfullscren="allowfullscren"></iframe>
+          <iframe style="width: 100%; height: 600px" src="https://stackblitz.com/edit/amexio-v4-simple-tree?embed=1&file=app/tree/simpletree/simpletree.demo.html" frameborder="0" allowfullscren="allowfullscren"></iframe>
           </amexio-tab>
         </amexio-tab-view>
       </amexio-body>
