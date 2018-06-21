f7aef822e4e25f9e2863042e83ebefd54e93a359
kedar8 <kedar.kokil@metamagic.in>
2018-06-21 18:59:58 +0530

    component :  stackblitz updated

--- src/app/navigation/notification/notification.demo.ts
+++ src/app/navigation/notification/notification.demo.ts
@@ -285,7 +285,7 @@ import {HttpClient} from "@angular/common/http";
           </amexio-tab>
           <amexio-tab title="Live">
            <p align="center">Amexio Sandbox</p>
-        <iframe style="width: 100%; height: 600px" src="https://stackblitz.com/edit/amexio-v4-notification?embed=1&file=app/navigation/notification/notification.demo.html&view=editor" frameborder="0" allowfullscren="allowfullscren"></iframe>
+        <iframe style="width: 100%; height: 600px" src="https://stackblitz.com/edit/amexio-v4-notification?embed=1&file=app/navigation/notification/notification.demo.html" frameborder="0" allowfullscren="allowfullscren"></iframe>
           </amexio-tab>
         </amexio-tab-view>
       </amexio-body>
