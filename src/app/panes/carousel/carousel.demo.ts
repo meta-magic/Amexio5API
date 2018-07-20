3345776b362996f7236cf6920c5ad894e75ba052
Manisha1205 <manisha.boddu@metamagic.in>
2018-07-20 12:10:32 +0530

    carousel events removed from apireference

--- src/app/panes/carousel/carousel.demo.ts
+++ src/app/panes/carousel/carousel.demo.ts
@@ -55,20 +55,6 @@ import {HttpClient} from "@angular/common/http";
               <amexio-data-table-column [width]="65" [data-index]="'description'" [data-type]="'string'" [hidden]="false"
                                         [text]="'Description'"></amexio-data-table-column>
            </amexio-datagrid>
-
-
-           <amexio-datagrid title="Events" [enable-column-fiter]="false"
-                            [http-method]="'get'"
-                            [http-url]="'assets/apireference/layout/carousel.json'"
-                            [data-reader]="'properties'"
-                            [enable-data-filter]="false" >
-             <amexio-data-table-column [width]="15" [data-index]="'name'" [data-type]="'string'" [hidden]="false"
-                                        [text]="'Name'"></amexio-data-table-column>
-              <amexio-data-table-column [width]="65" [data-index]="'description'" [data-type]="'string'" [hidden]="false"
-                                        [text]="'Description'"></amexio-data-table-column>
-           </amexio-datagrid>
-
-
          </amexio-tab>
          <amexio-tab title="Source">
            <div style="overflow-y: scroll">
