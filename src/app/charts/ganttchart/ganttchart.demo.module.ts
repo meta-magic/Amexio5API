5879ff3683f1a8cb861e8264b74c204b47c7d646
sagarjadhav968 <sagarjdhv968@gmail.com>
2018-07-24 17:26:52 +0530

    Gantt Chart : Gantt chart added

---
+++ src/app/charts/ganttchart/ganttchart.demo.module.ts
@@ -0,0 +1,17 @@
+import {RouterModule} from "@angular/router";
+import {AmexioChartsModule, AmexioWidgetModule} from "amexio-ng-extensions";
+import {FormsModule} from "@angular/forms";
+import {CommonModule} from "@angular/common";
+import {NgModule} from "@angular/core";
+import {SharedModule} from "../../shared.module";
+import {HttpClientModule} from "@angular/common/http";
+import { GanttChartDemoComponent } from "./ganttchart.demo.component";
+@NgModule({
+  declarations: [GanttChartDemoComponent],
+  imports: [CommonModule, FormsModule, HttpClientModule, AmexioWidgetModule, SharedModule,AmexioChartsModule,RouterModule.forChild([{
+    path: '', component: GanttChartDemoComponent, pathMatch: 'full'
+  }])],
+  providers: []
+})
+export class GanttChartDemoModule {
+}
