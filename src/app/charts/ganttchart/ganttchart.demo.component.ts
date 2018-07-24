96b5ab799c8f307740bc06b0c55d017609bcafe2
sagarjadhav968 <sagarjdhv968@gmail.com>
2018-07-24 18:49:20 +0530

    new changes

--- src/app/charts/ganttchart/ganttchart.demo.component.ts
+++ src/app/charts/ganttchart/ganttchart.demo.component.ts
@@ -47,13 +47,13 @@ import {HttpClient} from "@angular/common/http";
                              [http-url]="'assets/apireference/charts/ganttchart.json'"
                              [data-reader]="'chart'"
                              [enable-data-filter]="false" >
-              <amexio-data-table-column [width]="15" [data-index]="'name'" [data-type]="'string'" [hidden]="false"
+              <amexio-data-table-column [width]="25" [data-index]="'name'" [data-type]="'string'" [hidden]="false"
                                         [text]="'Name'"></amexio-data-table-column>
               <amexio-data-table-column [width]="10" [data-index]="'type'" [data-type]="'string'" [hidden]="false"
                                         [text]="'Type'"></amexio-data-table-column>
               <amexio-data-table-column [width]="10" [data-index]="'default'" [data-type]="'string'" [hidden]="false"
                                         [text]="'Default'"></amexio-data-table-column>
-              <amexio-data-table-column [width]="65" [data-index]="'description'" [data-type]="'string'" [hidden]="false"
+              <amexio-data-table-column [width]="55" [data-index]="'description'" [data-type]="'string'" [hidden]="false"
                                         [text]="'Description'"></amexio-data-table-column>
             </amexio-datagrid>
 
