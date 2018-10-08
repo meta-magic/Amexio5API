120c73521e95852638aeb7f0cdc275f64ffb1fd4
kedar8 <kedar.kokil@metamagic.in>
2018-10-08 18:55:28 +0530

    breadcrumb: stackblitz updated

--- src/app/navigation/breadcrumb/breadcrumb.demo.ts
+++ src/app/navigation/breadcrumb/breadcrumb.demo.ts
@@ -100,7 +100,7 @@ import { HttpClient } from "@angular/common/http";
           </amexio-tab>
           <amexio-tab title="Live">
           <p align="center">Amexio Sandbox</p>
-          <iframe style="width: 100%; height: 600px" src="https://stackblitz.com/edit/amexio-v4-listbox?embed=1&file=app/data/listbox/listbox.demo.component.ts" frameborder="0" allowfullscren="allowfullscren"></iframe>
+          <iframe style="width: 100%; height: 600px" src="https://stackblitz.com/edit/amexio-breadcrumb-demo?embed=1&file=src/app/breadcrumb/breadcrumb.demo.component.html&view=editor" frameborder="0" allowfullscren="allowfullscren"></iframe>
           </amexio-tab>
         </amexio-tab-view>
       </amexio-body>
