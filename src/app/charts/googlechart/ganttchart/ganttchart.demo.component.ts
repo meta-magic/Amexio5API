91fe68216777c94bb3de86d0946936b76908659e
rashmithakkar <rashmi.thakkar@metamagic.in>
2018-10-12 21:38:52 +0530

    Gantt chart: stackblitz added

--- src/app/charts/googlechart/ganttchart/ganttchart.demo.component.ts
+++ src/app/charts/googlechart/ganttchart/ganttchart.demo.component.ts
@@ -82,7 +82,7 @@ import {HttpClient} from "@angular/common/http";
           </amexio-tab>
           <amexio-tab title="Live">
            <p align="center">Amexio Sandbox</p>
-            WORK IN PROGRESS
+           <iframe style="width: 100%; height: 600px" src="https://stackblitz.com/edit/amexio-gantt-chart?embed=1&file=app/charts/timelinechart/ganttchart.demo.html&view=editor" frameborder="0" allowfullscren="allowfullscren"></iframe>
            </amexio-tab>
         </amexio-tab-view>
       </amexio-body>
