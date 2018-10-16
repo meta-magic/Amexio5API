dc0d628b816a36ab69c826f21aedd877cfa68ca2
medha410 <medha.jadhav@metamagic.in>
2018-10-16 16:59:15 +0530

    drillable api changes

--- src/app/charts/D3charts/d3drillablecharts/d3drillablecharts.demo.component.ts
+++ src/app/charts/D3charts/d3drillablecharts/d3drillablecharts.demo.component.ts
@@ -10,100 +10,28 @@ export class AmexioD3DrillableChartDemoComponent implements OnInit {
 
   htmlCode: string;
   typeScriptCode: string;
-  userDataSourceCode: string;
-  dataSourceCode: string;
   copyMsgArray: any[];
-  userDefineColors: any;
-  defultColorData: any;
-  userDefineColorData: any;
-  sourceData: any;
-  userDataSource: any;
-  barChartWithColorData: any;
-  barData: any;
-  sourceData1: any;
   drillabledata:any;
+  userDefineColors: any;
   constructor(private http: HttpClient) {
     this.getHtmlAndTypeScriptCode();
   }
 
 
   ngOnInit() {
-    this.userDefineColors =
-      [
-        "red",
-        "green",
-        "yellow",
-        "blue",
-        "pink",
-        "brown"
-      ]
-    this.defultColorData = [
-      ['year', 'performance'],
-      ['2011', 1020],
-      ['2012', 590],
-      ['2013', 1260],
-      ['2014', 830],
-      ['2015', 1030],
-      ['2016', 209],
-      ['2017', 1006]
-    ];
-
-    this.userDefineColorData =
-    [
-      ['city', 'population'],
-      ['Mumbai', 94423],
-      ['Kochi', 69932],
-      ['Delhi', 91345],
-      ['Chennai', 46467],
-      ['Pune', 31244],
-      ['Jaipur', 30461],
-      ['Ranchi', 84436]
-    ];
-
-      this.barData = 
-      [
-        ['year', 'production'],
-        ['2011', 300],
-        ['2012', 500],
-        ['2013', 700],
-        ['2014', 400],
-        ['2015', 600],
-        ['2016', 500],
-        ['2017', 200]
-      ];
-
      this.drillabledata=["label","value","name"];
+    
+     this.userDefineColors =
+      [
+        "#4040a1",
+        "#e06377",
+        " #7e4a35",
+        "#6b5b95",
+        "#feb236",
+        "#d64161",
+        "#ff7b25"
+      ]
        
-  }
-
-  onDefaultLegendClick(event: any) {
-
-    this.sourceData = event;
-
-  }
-
-  onDefaultLegendClick1(event: any) {
-this.sourceData1 = event;
-  }
-
-
-
-  onDefaultChartClick1(event: any) {
-    this.sourceData = event;
-  }
-
-  onDefaultChartClick2(event: any) {
-    this.sourceData1 = event;
-   }
-  
-
-  onDefaultChartClick(event: any) {
-    this.userDataSource = event;
-  }
-
-  onUserDefineLegendClick(event: any) {
-
-    this.userDataSource = event;
   }
 
   //TO LOAD HTML AND TYPESCRIPT CODE
@@ -112,7 +40,7 @@ this.sourceData1 = event;
     let responseTs: any;
     let dataSource: any;
     //HTML FILE
-    this.http.get('assets/data/code/charts/D3Charts/d3barchart/d3chart.html', { responseType: 'text' }).subscribe(data => {
+    this.http.get('assets/data/code/charts/D3Charts/d3drillablechart/d3chart.html', { responseType: 'text' }).subscribe(data => {
       responseHtml = data;
     }, error => {
     }, () => {
@@ -120,28 +48,13 @@ this.sourceData1 = event;
     });
 
     // TS FILE
-    this.http.get('assets/data/code/charts/D3Charts/d3barchart/d3chart.text', { responseType: 'text' }).subscribe(data => {
+    this.http.get('assets/data/code/charts/D3Charts/d3drillablechart/d3chart.text', { responseType: 'text' }).subscribe(data => {
       responseTs = data;
     }, error => {
     }, () => {
       this.typeScriptCode = responseTs;
     });
-    // DataSource FILE
-    this.http.get('assets/data/code/charts/D3Charts/d3barchart/datasource.json', { responseType: 'text' }).subscribe(data => {
-      responseTs = data;
-    }, error => {
-    }, () => {
-      this.dataSourceCode = responseTs;
-    });
-
-    // User DataSource FILE
-    this.http.get('assets/data/code/charts/D3Charts/d3barchart/usersource.json', { responseType: 'text' }).subscribe(data => {
-      responseTs = data;
-    }, error => {
-    }, () => {
-      this.userDataSourceCode = responseTs;
-    });
-
+   
   }
 
   //this code use copy to html code from tabpanel
