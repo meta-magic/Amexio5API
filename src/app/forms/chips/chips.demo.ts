3f9f4473edaccc6d56e768df3be43655e85f0085
ankita202 <ankita.jain@metamgic.in>
2018-10-25 15:15:40 +0530

    Chip:Component Enhancement

--- src/app/forms/chips/chips.demo.ts
+++ src/app/forms/chips/chips.demo.ts
@@ -9,7 +9,7 @@ import { HttpClient } from "@angular/common/http";
     selector: 'chips-demo', template: `
     <amexio-card header="true">
       <amexio-header>
-         Chips
+         Chips 
       </amexio-header>
       <amexio-body>
         <p>Chips allow users to enter information, make selections, filter content, or trigger actions. Chips appear dynamically as a group of multiple interactive elements.</p>
@@ -20,13 +20,26 @@ import { HttpClient } from "@angular/common/http";
               <amexio-column size="12">
               <amexio-card header="true">
                 <amexio-header>
-                  Chips
+                  Chips  with Data Input
                 </amexio-header>
                 <amexio-body>
-                <amexio-chips [data] = "sampledata" (onLabelClick)="onLabelClick($event)" (onCloseClick)="onCloseClick($event)"></amexio-chips>
-                <pre><code>{{emitdata | json}}</code></pre>
+                <amexio-chips [data] = "sampledatachips" (selectedchipsData)="onChipsLabelClick($event)"(onCloseClick)="onCloseClick($event)"></amexio-chips>
+                <pre><code>{{emitdatachips | json}}</code></pre>
                 </amexio-body>
               </amexio-card>
+              <amexio-card header="true">
+                <amexio-header>
+                  Chip with multiple Inputs 
+                </amexio-header>
+                <amexio-body>
+                <amexio-chips *ngFor="let item of sampledatachip">
+                      <amexio-chip [icon]="item.icon" [label]= "item.label" [color]= "item.color" [badge]= "item.badge" [closable]= "item.closable" (labelClick)="onChipLabelClick($event)" (closeClick)="onCloseClick($event)">
+                      </amexio-chip>
+                </amexio-chips>
+                <pre><code>{{emitdatachip | json}}</code></pre> 
+                </amexio-body>
+              </amexio-card>
+              
             </amexio-column>
             </amexio-row>
 
@@ -47,10 +60,36 @@ import { HttpClient } from "@angular/common/http";
                                         [text]="'Default'"></amexio-data-table-column>
               <amexio-data-table-column [width]="45" [data-index]="'description'" [data-type]="'string'" [hidden]="false"
                                         [text]="'Description'"></amexio-data-table-column>
+              </amexio-datagrid>
+            <amexio-datagrid title="Properties<amexio-chip>" [enable-column-fiter]="false"
+              [http-method]="'get'"
+              [http-url]="'assets/apireference/forms/chip.json'"
+              [data-reader]="'properties'"
+              [enable-data-filter]="false">
+              <amexio-data-table-column [width]="15" [data-index]="'name'" [data-type]="'string'" [hidden]="false"
+                        [text]="'Name'"></amexio-data-table-column>
+              <amexio-data-table-column [width]="15" [data-index]="'version'" [data-type]="'string'" [hidden]="false"
+                        [text]="'Version'"></amexio-data-table-column>
+              <amexio-data-table-column [width]="15" [data-index]="'type'" [data-type]="'string'" [hidden]="false"
+                        [text]="'Type'"></amexio-data-table-column>
+              <amexio-data-table-column [width]="15" [data-index]="'default'" [data-type]="'string'" [hidden]="false"
+                        [text]="'Default'"></amexio-data-table-column>
+              <amexio-data-table-column [width]="45" [data-index]="'description'" [data-type]="'string'" [hidden]="false"
+                        [text]="'Description'"></amexio-data-table-column>
             </amexio-datagrid>
             <br>
-            <amexio-datagrid title="Events" [http-method]="'get'"
+            <amexio-datagrid title="Events<amexio-chips>" [http-method]="'get'"
                              [http-url]="'assets/apireference/forms/chips.json'" [data-reader]="'events'"
+                             [enable-data-filter]="false">
+              <amexio-data-table-column [width]="20"[data-index]="'name'" [data-type]="'string'" [hidden]="false"
+                                        [text]="'Name'"></amexio-data-table-column>
+              <amexio-data-table-column [width]="15" [data-index]="'version'" [data-type]="'string'" [hidden]="false"
+                                        [text]="'Version'"></amexio-data-table-column>
+              <amexio-data-table-column [width]="80" [data-index]="'description'" [data-type]="'string'" [hidden]="false"
+                                        [text]="'Description'"></amexio-data-table-column>
+            </amexio-datagrid>
+            <amexio-datagrid title="Events<amexio-chip>" [http-method]="'get'"
+                             [http-url]="'assets/apireference/forms/chip.json'" [data-reader]="'events'"
                              [enable-data-filter]="false">
               <amexio-data-table-column [width]="20"[data-index]="'name'" [data-type]="'string'" [hidden]="false"
                                         [text]="'Name'"></amexio-data-table-column>
@@ -102,40 +141,88 @@ export class ChipsDemo {
     checkboxGroupdata: any;
     checkboxGroupdatadisabled: any;
     checkboxGroupdatadisabledallitem: any;
-    sampledata: any;
-    emitdata: any;
+    sampledatachips: any;
+    sampledatachip:any;
+    emitdatachips: any;
+    emitdatachip: any;
     constructor(private http: HttpClient) {
-      this.sampledata = [{
-        icon: 'fa fa-camera',
-        label: 'Camera',
+      this.sampledatachips = [{
+        icon: 'fa fa-home',
+        label: 'Home',
+        color: 'red',
+        badge: 4,
         closable: true
       },
       {
-        icon: 'fa fa-fire-extinguisher',
-        label: "Fire-Safety",
+        icon: 'fa fa-download',
+        label: "Download",
+        color:'blue',
+        badge: 5,
         closable: false
       }, {
-        icon: 'fa fa-wifi',
-        label: "Wifi",
+        icon: 'fa fa-camera',
+        label: "Camera",
+        color: 'green',
+        badge: 6,
         closable: true
       }, {
-        icon: 'fa fa-taxi',
-        label: "Parking-Area",
-        closable: false
+        icon: 'fa fa-fire-extinguisher',
+        label: "Fire Extinguisher",
+        color: 'yellow',
+        badge: 7,
+        closable: true
       }, {
         icon: 'fa fa-ambulance',
         label: "Emergency",
+        color: 'pink',
+        badge: 8,
+        closable: true
+      }
+      ]
+      this.sampledatachip = [{
+        icon: 'fa fa-linkedin-square',
+        label: 'Linkedin',
+        color: 'red',
+        badge: 4,
+        closable: true
+      },
+      {
+        icon: 'fa fa-internet-explorer',
+        label: "Internet-Explorer",
+        color:'blue',
+        badge: 5,
+        closable: false
+      }, {
+        icon: 'fa fa-github',
+        label: "Github",
+        color: 'green',
+        badge: 6,
+        closable: true
+      }, {
+        icon: 'fa fa-twitter',
+        label: "Twitter",
+        color: 'yellow',
+        badge: 7,
+        closable: true
+      }, {
+        icon: 'fa fa-reddit',
+        label: "Reddit",
+        color: '',
+        badge: 8,
         closable: true
       }
       ]
         this.getHtmlAndTypeScriptCode();
     }
-    onLabelClick(event: any){
-      this.emitdata = event;
+    onChipsLabelClick(event: any){
+      this.emitdatachips  = event;
+    }
+    onChipLabelClick(event: any){
+      this.emitdatachip = event;
     }
 
     onCloseClick(event: any){
-      this.emitdata = event;
+      this.emitdatachips = event;
     }
 
     //TO LOAD HTML AND TYPESCRIPT CODE
