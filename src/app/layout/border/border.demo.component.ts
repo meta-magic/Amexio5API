484f32abedd4bfcca6cf3697143264e0ea75d239
kedar8 <kedar.kokil@metamagic.in>
2018-11-15 11:15:48 +0530

    border and layout : code updated

--- src/app/layout/border/border.demo.component.ts
+++ src/app/layout/border/border.demo.component.ts
@@ -2,104 +2,18 @@
  * Created by pratik on 16/1/18.
  */
 import { Component, OnInit } from '@angular/core';
-import {HttpClient} from "@angular/common/http";
+import { HttpClient } from "@angular/common/http";
 
 @Component({
- selector: 'border-demo',
- template: `
-   <amexio-card header="true">
-     <amexio-header>
-        Border Layout 
-     </amexio-header>
-     <amexio-body>
-       <p>A border layout lays out a container, arranging and resizing its components to fit in five regions: north, south, east, west, and center.</p>
-       <amexio-tab-view>
-         <amexio-tab title="Demo" active="true">
-           <amexio-card header="true">
-             <amexio-header> Border Layout</amexio-header>
-             <amexio-body>
-               <amexio-borderlayout>
-                 <amexio-borderlayout-item position="north">
-                   <amexio-card><amexio-body>
-                     Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-                   </amexio-body></amexio-card>
-                 </amexio-borderlayout-item>
-                 <amexio-borderlayout-item position="east">
-                   <amexio-card><amexio-body>
-                     Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-                  
-                   </amexio-body></amexio-card>
-                 </amexio-borderlayout-item>
-                 <amexio-borderlayout-item position="center">
-                   <amexio-card><amexio-body>
-                     Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-                     Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-                     Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                   </amexio-body></amexio-card>
-                 </amexio-borderlayout-item>
-                 <amexio-borderlayout-item position="west">
-                   <amexio-card><amexio-body> 
-                     Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-                   </amexio-body></amexio-card>
-                 </amexio-borderlayout-item>
-                 <amexio-borderlayout-item position="south">
-                   <amexio-card><amexio-body> 
-                     Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-                   </amexio-body></amexio-card>
-                 </amexio-borderlayout-item>
-               </amexio-borderlayout>
-             </amexio-body>
-           </amexio-card>
-          
-         </amexio-tab>
-         <amexio-tab title="API Reference">
-            <amexio-datagrid title="Properties <amexio-borderlayout-item>" [enable-column-fiter]="false"
-           [http-method]="'get'"
-           [http-url]="'assets/apireference/border/borderlayoutitem.component.json'"
-           [data-reader]="'properties'"
-           [enable-data-filter]="false" >
-<amexio-data-table-column [width]="15" [data-index]="'name'" [data-type]="'string'" [hidden]="false"
-                       [text]="'Name'"></amexio-data-table-column>
-<amexio-data-table-column [width]="10" [data-index]="'type'" [data-type]="'string'" [hidden]="false"
-                       [text]="'Type'"></amexio-data-table-column>
-<amexio-data-table-column [width]="10" [data-index]="'default'" [data-type]="'string'" [hidden]="false"
-                       [text]="'Default'"></amexio-data-table-column>
-<amexio-data-table-column [width]="65" [data-index]="'description'" [data-type]="'string'" [hidden]="false"
-                       [text]="'Description'"></amexio-data-table-column>
-</amexio-datagrid>
-         </amexio-tab>
-         <amexio-tab title="Source">
-           <div style="overflow-y: scroll">
-           <amexio-vertical-tab-view>
-             <amexio-tab title="HTML" [active]="true">
-               <ng-container *ngIf="htmlCode">
-                 <!--<clip-copy [htmlCode]="htmlCode" (onClick)="onCopyClick()"></clip-copy>-->
-                 <prism-block [code]="htmlCode" [language]="'html'"></prism-block>
-               </ng-container>
-             </amexio-tab>
-             <amexio-tab title="Type Script">
-               <ng-container *ngIf="typeScriptCode">
-                 <prism-block [code]="typeScriptCode" [language]="'typescript'"></prism-block>
-               </ng-container>
-             </amexio-tab>
-           </amexio-vertical-tab-view>
-           </div>
-         </amexio-tab>
-         <amexio-tab title="Live">
-         <p align="center">Amexio Sandbox</p>
-         <iframe style="width: 100%; height: 600px" src="https://stackblitz.com/edit/amexio-v4-borderlayout?embed=1&file=app/layouts/borderlayout/borderlayout.demo.html" frameborder="0" allowfullscren="allowfullscren"></iframe>
-         </amexio-tab>
-       </amexio-tab-view>
-     </amexio-body>
-   </amexio-card>
- `
+  selector: 'border-demo',
+  templateUrl: './border.demo.html',
 })
 
 export class BorderDemoComponent {
   htmlCode: string;
   typeScriptCode: string;
   copyMsgArray: any[];
-  asyncFlag : boolean;
+  asyncFlag: boolean;
   constructor(private http: HttpClient) {
     this.getHtmlAndTypeScriptCode();
   }
@@ -115,7 +29,7 @@ export class BorderDemoComponent {
     let responseTs: any;
 
     //HTML FILE
-    this.http.get('assets/data/code/border/border.html',{responseType: 'text'}).subscribe(data => {
+    this.http.get('assets/data/code/border/border.html', { responseType: 'text' }).subscribe(data => {
       responseHtml = data;
     }, error => {
     }, () => {
@@ -123,7 +37,7 @@ export class BorderDemoComponent {
     });
 
     //TS FILE
-    this.http.get('assets/data/code/border/border.text',{responseType: 'text'}).subscribe(data => {
+    this.http.get('assets/data/code/border/border.text', { responseType: 'text' }).subscribe(data => {
       responseTs = data;
     }, error => {
     }, () => {
@@ -136,9 +50,9 @@ export class BorderDemoComponent {
   onCopyClick() {
     if (this.copyMsgArray.length >= 1) {
       this.copyMsgArray = [];
-      this.copyMsgArray.push({'msg': 'Code Copied', 'type': 'info'});
+      this.copyMsgArray.push({ 'msg': 'Code Copied', 'type': 'info' });
     } else {
-      this.copyMsgArray.push({'msg': 'Code Copied', 'type': 'info'});
+      this.copyMsgArray.push({ 'msg': 'Code Copied', 'type': 'info' });
     }
   }
 }
