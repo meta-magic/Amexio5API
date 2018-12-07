eeffd85504375d29587de0c8717609a228ac6945
ankita202 <ankita.jain@metamgic.in>
2018-12-07 18:59:01 +0530

    SearchBox:updated

--- src/app/forms/searchbox/searchbox.demo.ts
+++ src/app/forms/searchbox/searchbox.demo.ts
@@ -91,6 +91,23 @@ import { HttpClient } from "@angular/common/http";
                                           [text]="'Description'"></amexio-data-table-column>
               </amexio-datagrid>
               <br>
+              <amexio-datagrid title="Properties <amexio-searchbox-options> " [enable-column-fiter]="false"
+                               [http-method]="'get'"
+                               [http-url]="'assets/apireference/forms/searchbox.json'" 
+                               [data-reader]="'childproperties'"
+                               [enable-data-filter]="false" >
+                <amexio-data-table-column [width]="15" [data-index]="'name'" [data-type]="'string'" [hidden]="false"
+                                          [text]="'Name'"></amexio-data-table-column>
+                <amexio-data-table-column [width]="20" [data-index]="'version'" [data-type]="'string'" [hidden]="false"
+                                          [text]="'Version'"></amexio-data-table-column>                                                  
+                <amexio-data-table-column [width]="10" [data-index]="'type'" [data-type]="'string'" [hidden]="false"
+                                          [text]="'Type'"></amexio-data-table-column>
+                <amexio-data-table-column [width]="10" [data-index]="'default'" [data-type]="'string'" [hidden]="false"
+                                          [text]="'Default'"></amexio-data-table-column>
+                <amexio-data-table-column [width]="65" [data-index]="'description'" [data-type]="'string'" [hidden]="false"
+                                          [text]="'Description'"></amexio-data-table-column>
+              </amexio-datagrid>
+              <br>
               <amexio-datagrid title="Events" [http-method]="'get'" 
                                [http-url]="'assets/apireference/forms/searchbox.json'" [data-reader]="'events'"
                                [enable-data-filter]="false">
