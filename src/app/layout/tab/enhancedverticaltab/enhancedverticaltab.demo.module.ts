8fd81e24053ab5591168dfd52d6ab477a831adc6
ankita202 <ankita.jain@metamgic.in>
2018-12-14 16:59:12 +0530

    Vertical Tab:Updated

--- src/app/layout/tab/enhancedverticaltab/enhancedverticaltab.demo.module.ts
+++ src/app/layout/tab/enhancedverticaltab/enhancedverticaltab.demo.module.ts
@@ -1,20 +1,22 @@
 import {NgModule} from "@angular/core";
 import {RouterModule} from "@angular/router";
-import {AmexioWidgetModule} from "amexio-ng-extensions";
+import {AmexioWidgetModule,AmexioPaneModule} from "amexio-ng-extensions";
 import {HttpModule} from "@angular/http";
 import {FormsModule} from "@angular/forms";
 import {CommonModule} from "@angular/common";
 import {SharedModule} from "../../../shared.module";
 import {EnhancedVerticalTabDemo} from "./enhancedverticaltab.demo";
+import {DyanmicVerticalTabComponent} from './dynamicverticaltabdemo.component';
 /**
  * Created by sagar on 11/1/18.
  */
 @NgModule({
-  declarations: [EnhancedVerticalTabDemo],
-  imports: [CommonModule, FormsModule, HttpModule,SharedModule, AmexioWidgetModule, RouterModule.forChild([{
+  declarations: [EnhancedVerticalTabDemo,DyanmicVerticalTabComponent],
+  imports: [CommonModule, FormsModule, HttpModule,SharedModule,AmexioPaneModule, AmexioWidgetModule, RouterModule.forChild([{
     path: '', component: EnhancedVerticalTabDemo, pathMatch: 'full'
   }])],
-  providers: []
+  providers: [],
+  entryComponents:[DyanmicVerticalTabComponent]
 })
 export class EnhancedVerticalTabDemoModule {
 
--- src/app/layout/tab/enhancedverticaltab/enhancedverticaltab.demo.ts
+++ src/app/layout/tab/enhancedverticaltab/enhancedverticaltab.demo.ts
@@ -2,11 +2,12 @@
  * Created by sagar on 9/1/18.
  */
 
-import {Component} from '@angular/core'
-import {Http} from "@angular/http";
+import { Component } from '@angular/core'
+import { Http } from "@angular/http";
+import { DyanmicVerticalTabComponent } from './dynamicverticaltabdemo.component';
 
 @Component({
-  selector: 'enhanced-vertical-tab-demo', templateUrl:'enhancedverticaltab.demo.html'
+  selector: 'enhanced-vertical-tab-demo', templateUrl: 'enhancedverticaltab.demo.html'
 })
 export class EnhancedVerticalTabDemo {
   htmlCode: string;
@@ -16,11 +17,26 @@ export class EnhancedVerticalTabDemo {
   constructor(private http: Http) {
     this.getHtmlAndTypeScriptCode();
   }
+  // title = 'component';
+  tabcountleft: number = 4;
+  tabcountright:number =4;
+  addtab(tab: any) {
+    this.tabcountleft++;
+    let title = 'Tab ' + this.tabcountleft;
+    let cmp = tab.addDynamicTab(title, "red", true, DyanmicVerticalTabComponent);
+    cmp.content = "Content of " + title;
+  }
+  addrighttab(tab1: any) {
+    this.tabcountright++;
+    let title = 'Tab ' + this.tabcountright;
+    let cmp = tab1.addDynamicTab(title, "red", true, DyanmicVerticalTabComponent);
+    cmp.content = "Content of " + title;
+  }
 
   //TO LOAD HTML AND TYPESCRIPT CODE
   getHtmlAndTypeScriptCode() {
     let responseHtml: any;
-    let responseTs:any;
+    let responseTs: any;
 
     //HTML FILE
     this.http.get('assets/data/code/layout/tab/enhancedverticaltab/tab.html').subscribe(data => {
@@ -44,11 +60,12 @@ export class EnhancedVerticalTabDemo {
   onCopyClick() {
     if (this.copyMsgArray.length >= 1) {
       this.copyMsgArray = [];
-      this.copyMsgArray.push({'msg': 'Code Copied', 'type': 'info'});
+      this.copyMsgArray.push({ 'msg': 'Code Copied', 'type': 'info' });
     } else {
-      this.copyMsgArray.push({'msg': 'Code Copied', 'type': 'info'});
+      this.copyMsgArray.push({ 'msg': 'Code Copied', 'type': 'info' });
     }
   }
+  
 }
 
 
