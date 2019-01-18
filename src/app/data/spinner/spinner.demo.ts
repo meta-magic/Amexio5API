6778b72f626b63374ec499d92d3384f6faacf1d5
kedar8 <kedar.kokil@metamagic.in>
2019-01-18 20:31:19 +0530

    spinner demo: updated.

--- src/app/data/spinner/spinner.demo.ts
+++ src/app/data/spinner/spinner.demo.ts
@@ -6,208 +6,80 @@ import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
 @Component({
-    selector: 'amexio-spinner-demo', template: `
-    <amexio-card header="true">
-      <amexio-header>
-      Data Loading Indicator
-      </amexio-header>
-      <amexio-body>
-        <p>Data Loading Indicator Component Provides up-to-date feedback on the progress of a workflow or action with simple yet flexible spinner and easy to configure.</p>
-        <amexio-tab-view>
-        <amexio-tab title="Demo" active="true">
-  <amexio-row>
-    <amexio-column size="12">
-    <amexio-card [header]="true">
-    <amexio-header>
-        Data Loading Indicator
-    </amexio-header>
-    <amexio-body>
-
-    <amexio-button 
-               [label]="text" 
-               [type]="'yellow'" 
-               [tooltip]="'show'" 
-               (onClick)="onButtonClick(click)">
-    </amexio-button>  
- <br/>
-    <amexio-label size="small-bold">Select Color For Spinner :  -</amexio-label>
-    <input type="color" id="head" name="color"
-                    (input)= "setColorForIndictor($event)"
-                    value="colorModel.color" />  
-    
-
-<amexio-row>
-    <amexio-column [size]="4" [fit]=true>
-        <amexio-card [header]="true" [body-height]="45">
-            <amexio-header>
-                fading circle round loader
-            </amexio-header>
-            <amexio-body>
-                <amexio-spinner #click [show]='clickShow' [size]="'18px'" [vertical-position]="'top'"
-                [horizontal-position]="'left'"
-                [type]="'fadingcircle'" [color]="colorModel.color">
-                </amexio-spinner>
-            </amexio-body>
-        </amexio-card>
-    </amexio-column>
-    <amexio-column [size]="4" [fit]=true>
-        <amexio-card [header]="true">
-            <amexio-header>
-                ball spin loader
-            </amexio-header>
-            <amexio-body>
-                <amexio-spinner [show]=clickShow [size]="'16px'" [vertical-position]="'top'"
-                [horizontal-position]="'center'" [type]="'ballspin'" [color]="colorModel.color">
-                </amexio-spinner>
-            </amexio-body>
-        </amexio-card>
-    </amexio-column>
-    <amexio-column [size]="4" [fit]=true>
-        <amexio-card [header]="true">
-            <amexio-header>
-                fire spin loader
-            </amexio-header>
-            <amexio-body>
-                <amexio-spinner [show]=clickShow [size]="'40px'"  [vertical-position]="'top'"
-                [horizontal-position]="'right'"
-                     [type]="'firespin'" [color]="colorModel.color">
-                </amexio-spinner>
-            </amexio-body>
-        </amexio-card>
-    </amexio-column>
-</amexio-row>
-
-<amexio-row>
-    <amexio-column [size]="4" [fit]=true>
-        <amexio-card [header]="true" [body-height]="45">
-            <amexio-header>
-                three bounce loader
-            </amexio-header>
-            <amexio-body>
-                <amexio-spinner [show]=clickShow [size]="'15px'" [vertical-position]="'center'"
-                [horizontal-position]="'left'" [type]="'threebounce'" [color]="colorModel.color">
-                </amexio-spinner>
-            </amexio-body>
-        </amexio-card>
-    </amexio-column>
-    <amexio-column [size]="4" [fit]=true>
-        <amexio-card [header]="true" [body-height]="45">
-            <amexio-header>
-                spinner round loader
-            </amexio-header>
-            <amexio-body>
-                <amexio-spinner [show]='clickShow' [size]="'8px'" [vertical-position]="'center'"
-                [horizontal-position]="'center'"[type]="'spinnerround'" [color]="colorModel.color">
-                </amexio-spinner>
-            </amexio-body>
-        </amexio-card>
-    </amexio-column>
-    <amexio-column [size]="4" [fit]=true>
-        <amexio-card [header]="true">
-            <amexio-header>
-                ring loader
-            </amexio-header>
-            <amexio-body>
-                <amexio-spinner [show]=clickShow [size]="'6px'" [vertical-position]="'center'"
-                [horizontal-position]="'right'" [type]="'ring'" [color]="colorModel.color">
-                </amexio-spinner>
-            </amexio-body>
-        </amexio-card>
-    </amexio-column>
-</amexio-row>
-
-<amexio-row>
-    <amexio-column [size]="4" [fit]=true>
-        <amexio-card [header]="true" [body-height]="45">
-            <amexio-header>
-                half circle loader
-            </amexio-header>
-            <amexio-body>
-                <amexio-spinner [show]=clickShow [size]="'9px'"  [vertical-position]="'bottom'"
-                [horizontal-position]="'left'"[type]="'halfcircle'">
-                </amexio-spinner>
-            </amexio-body>
-        </amexio-card>
-    </amexio-column>
-
-    <amexio-column [size]="4" [fit]=true>
-        <amexio-card [header]="true" [body-height]="45">
-            <amexio-header>
-                rectangle Bounce loader
-            </amexio-header>
-            <amexio-body>
-                <amexio-spinner [show]=clickShow [size]="'16px'" [vertical-position]="'bottom'"
-                [horizontal-position]="'center'" [type]="'rectanglebounce'" >
-                </amexio-spinner>
-            </amexio-body>
-        </amexio-card>
-    </amexio-column>
-</amexio-row>
-
-
-
-       
-    </amexio-body>
-</amexio-card>
-
-    </amexio-column>
-  </amexio-row>
-</amexio-tab>
-          <amexio-tab title="API Reference">
-            <amexio-datagrid title="Properties<amexio-spinner>" [enable-column-fiter]="false"
-                             [http-method]="'get'"
-                             [http-url]="'assets/apireference/data/spinner.json'"
-                             [data-reader]="'properties'"
-                             [enable-data-filter]="false">
-              <amexio-data-table-column [data-index]="'name'" [width]="15" [data-type]="'string'" [hidden]="false"
-                                        [text]="'Name'"></amexio-data-table-column>
-              <amexio-data-table-column [width]="10" [data-index]="'version'" [data-type]="'string'" [hidden]="false"
-                                        [text]="'Version'"></amexio-data-table-column>
-              <amexio-data-table-column [data-index]="'type'" [width]="10" [data-type]="'string'" [hidden]="false"
-                                        [text]="'Type'"></amexio-data-table-column>
-              <amexio-data-table-column [data-index]="'default'" [width]="10" [data-type]="'string'" [hidden]="false"
-                                        [text]="'Default'"></amexio-data-table-column>
-              <amexio-data-table-column [data-index]="'description'" [width]="65" [data-type]="'string'" [hidden]="false"
-                                        [text]="'Description'"></amexio-data-table-column>
-            </amexio-datagrid>
-          </amexio-tab>
-          <amexio-tab title="Source">
-            <div style="overflow-y: scroll">
-            <amexio-vertical-tab-view>
-              <amexio-tab title="HTML" [active]="true">
-                <ng-container *ngIf="htmlCode">
-                  <!--<clip-copy [htmlCode]="htmlCode" (onClick)="onCopyClick()"></clip-copy>-->
-                  <prism-block [code]="htmlCode" [language]="'html'"></prism-block>
-                </ng-container>
-              </amexio-tab>
-              <amexio-tab title="Type Script">
-                <ng-container *ngIf="typeScriptCode">
-                  <prism-block [code]="typeScriptCode" [language]="'typescript'"></prism-block>
-                </ng-container>
-              </amexio-tab>
-            </amexio-vertical-tab-view>
-            </div>
-          </amexio-tab>
-          <amexio-tab title="Live">
-                      <p align="center">Amexio Sandbox</p>
-<iframe style="width: 100%; height: 600px" src="https://stackblitz.com/edit/amexio-spinner?embed=1&file=src/app/spinner/spinner.demo.component.html&view=editor" frameborder="0" allowfullscren="allowfullscren"></iframe>
-          </amexio-tab>
-        </amexio-tab-view>
-      </amexio-body>
-    </amexio-card>
-  `
+    selector: 'amexio-spinner-demo',
+    templateUrl: './spinner.demo.html',
+  
 })
 export class AmexioSpinnerDemo {
     htmlCode: string;
     typeScriptCode: string;
     copyMsgArray: any[];
     color: any;
+    spinnerData: any;
+    AdvanceBoxShow: boolean = true;
     colorModel: ColorModel;
     clickShow: boolean = true;
+    advanceData: string = 'Hide';
     text: string = 'Hide';
+
+    spinnerType: string;
+    hposition: string;
+    vposition: string;
+
     constructor(private http: HttpClient) {
         this.getHtmlAndTypeScriptCode();
         this.colorModel = new ColorModel();
+
+        this.spinnerData = {
+            "type": [
+              {
+                "spinnerType": "rectanglebounce",
+              },
+              {
+                "spinnerType": "ring",
+              },
+              {
+                "spinnerType": "halfcircle",
+              },
+              {
+                "spinnerType": "fadingcircle",
+              },
+              {
+                "spinnerType": "ballspin",
+              },
+              {
+                "spinnerType": "firespin",
+              },
+              {
+                "spinnerType": "threebounce",
+              },
+              {
+                "spinnerType": "spinnerround",
+              }
+            ],
+            "horizontalPosition": [
+              {
+                "hposition": "left"
+              },
+              {
+                "hposition": "center"
+              },
+              {
+                "hposition": "right"
+              }
+            ],
+            "verticalPosition": [
+              {
+                "vposition": "top"
+              },
+              {
+                "vposition": "center"
+              },
+              {
+                "vposition": "bottom"
+              }
+            ]
+          }
     }
 
     //TO LOAD HTML AND TYPESCRIPT CODE
@@ -247,8 +119,8 @@ export class AmexioSpinnerDemo {
         setColorForIndictor(colorName: any) {
             this.colorModel.color = colorName.target.value;
         }
-
-    onButtonClick(click: any) {
+    // Method For the spinner show and hide
+    onButtonClick() {
 
         if (this.clickShow) {
             this.text = 'Show';
@@ -256,6 +128,15 @@ export class AmexioSpinnerDemo {
             this.text = 'Hide'
         }
         this.clickShow = !this.clickShow;
+    }
+
+    onAdvanceClick() {
+        if(this.AdvanceBoxShow) {
+            this.advanceData = 'Show';
+        } else {
+            this.advanceData =' Hide';
+        }
+        this.AdvanceBoxShow = !this.AdvanceBoxShow;
     }
 }
 
