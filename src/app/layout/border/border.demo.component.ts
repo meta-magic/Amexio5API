5f9340f8e185968eed11edfc2c14590b147cc727
kedar8 <kedar.kokil@metamagic.in>
2019-02-01 19:19:25 +0530

    border: updated html code demo

--- src/app/layout/border/border.demo.component.ts
+++ src/app/layout/border/border.demo.component.ts
@@ -2,7 +2,6 @@
  * Created by pratik on 16/1/18.
  */
 import { Component, OnInit } from '@angular/core';
-import { HttpClient } from "@angular/common/http";
 
 @Component({
   selector: 'border-demo',
@@ -10,42 +9,12 @@ import { HttpClient } from "@angular/common/http";
 })
 
 export class BorderDemoComponent {
-  htmlCode: string;
-  typeScriptCode: string;
+ 
   copyMsgArray: any[];
-  asyncFlag: boolean;
-  constructor(private http: HttpClient) {
-    this.getHtmlAndTypeScriptCode();
+  
+  constructor() {
   }
-  getDta() {
-    this.asyncFlag = true;
-    setTimeout(() => {
-      this.asyncFlag = false;
-    }, 3000);
-  }
-  //TO LOAD HTML AND TYPESCRIPT CODE
-  getHtmlAndTypeScriptCode() {
-    let responseHtml: any;
-    let responseTs: any;
-
-    //HTML FILE
-    this.http.get('assets/data/code/border/border.html', { responseType: 'text' }).subscribe(data => {
-      responseHtml = data;
-    }, error => {
-    }, () => {
-      this.htmlCode = responseHtml;
-    });
-
-    //TS FILE
-    this.http.get('assets/data/code/border/border.text', { responseType: 'text' }).subscribe(data => {
-      responseTs = data;
-    }, error => {
-    }, () => {
-      this.typeScriptCode = responseTs;
-    });
-
-  }
-
+ 
   //THIS METHOD USED FOR COPY THE HTML & TYPESCRIPT CODE
   onCopyClick() {
     if (this.copyMsgArray.length >= 1) {
