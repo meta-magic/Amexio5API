c68df927dda51af5756397eac9a3a96b7ef0e2aa
datsgawas <dattaram.gawas@metamagic.in>
2019-02-01 17:35:22 +0530

    amexio structure updated.

--- src/app/amexioStructureDemo/amexiostructure/amexiostructure.component.ts
+++ src/app/amexioStructureDemo/amexiostructure/amexiostructure.component.ts
@@ -1,57 +1,35 @@
 
-import { Component, OnInit, Input, QueryList, ContentChildren, AfterContentInit } from '@angular/core';
-import { AmexioApiReferenceComponent } from '../apiReference/amexioapireference.component';
-import { AmexioApiSourceCodeComponent } from '../apiSourceCode/apisourcecode.component';
-import { AmexioApiLiveDemoComponent } from '../apiLiveDemo/apilivedemo.component';
+import { Component, OnInit, Input} from '@angular/core';
+import {RestCallService} from '../../services/restcall.service';
 @Component({
   selector: 'amexio-api-structure',
   templateUrl: 'amexiostructure.component.html',
 })
 
-export class AmexioStructureComponent implements OnInit, AfterContentInit {
+export class AmexioStructureComponent implements OnInit {
 
   @Input('title') title: string;
 
   @Input('description') description: string;
 
-// 
-  amexoiApiRef: AmexioApiReferenceComponent;
-  @ContentChildren(AmexioApiReferenceComponent) apiRefernce: QueryList<AmexioApiReferenceComponent>;
-  listOfAPIReference: AmexioApiReferenceComponent[];
+  @Input('url') url: string;
 
-  @ContentChildren(AmexioApiSourceCodeComponent) apiSourceCode: QueryList<AmexioApiSourceCodeComponent>;
-  listOfApiSourceCode: AmexioApiSourceCodeComponent[];
-  amexioApiSourceCode: AmexioApiSourceCodeComponent;
+  comData: any;
 
-  @ContentChildren(AmexioApiLiveDemoComponent) apiLiveDemo: QueryList<AmexioApiLiveDemoComponent>;
-  listOfLiveDemoComponent: AmexioApiLiveDemoComponent[];
-  liveDemoComponent: AmexioApiLiveDemoComponent;
-
-  constructor() {
+  constructor(private _rCService: RestCallService) {
 
 
-  }
-
-
-  ngAfterContentInit() {
-    this.listOfAPIReference = this.apiRefernce.toArray();
-    if (this.listOfAPIReference && this.listOfAPIReference.length > 0) {
-      this.amexoiApiRef = this.listOfAPIReference[0];
-    }
-
-    this.listOfApiSourceCode = this.apiSourceCode.toArray();
-    if (this.listOfApiSourceCode && this.listOfApiSourceCode.length > 0) {
-      this.amexioApiSourceCode = this.listOfApiSourceCode[0];
-    }
-
-    this.listOfLiveDemoComponent = this.apiLiveDemo.toArray();
-    if (this.listOfLiveDemoComponent && this.listOfLiveDemoComponent.length > 0) {
-      this.liveDemoComponent = this.listOfLiveDemoComponent[0];
-    }
   }
   ngOnInit() {
-
+    if (this.url) {
+      this.getComponentData();
+    }
   }
 
-
+  getComponentData() {
+    this._rCService.getCall(this.url).subscribe(
+      (res: any) => {
+        this.comData = res;
+      });
+  }
 }
--- src/app/layout/box/box.demo.ts
+++ src/app/layout/box/box.demo.ts
@@ -2,7 +2,6 @@
  * Created by anaghak07 on 13/3/18.
  */
 import {Component, OnInit} from '@angular/core';
-import {HttpClient} from "@angular/common/http";
 import {RestCallService} from "../../services/restcall.service";
 
 @Component({
@@ -11,7 +10,6 @@ import {RestCallService} from "../../services/restcall.service";
 })
 export class BoxDemo implements OnInit{
   copyMsgArray: any[];
-  age:number=10;
   comData: any;
   constructor(private _rCService: RestCallService) {}
 
