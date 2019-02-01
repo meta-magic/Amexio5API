effdaf0f719b7fbe193402f8be83dec6cbf948b8
kedar8 <kedar.kokil@metamagic.in>
2019-02-01 19:57:19 +0530

    panel: code updated.

--- src/app/panes/panel/panel.demo.ts
+++ src/app/panes/panel/panel.demo.ts
@@ -2,7 +2,6 @@
  * Created by pratik on 16/1/18.
  */
 import { Component, OnInit } from '@angular/core';
-import { HttpClient } from "@angular/common/http";
 
 @Component({
   selector: 'panel-demo',
@@ -10,47 +9,19 @@ import { HttpClient } from "@angular/common/http";
 })
 
 export class PanelDemoComponent {
-  htmlCode: string;
-  typeScriptCode: string;
+ 
   copyMsgArray: any[];
-  asyncFlag: boolean;
+ 
   refreshDialogue: boolean;
   rightclickdata: any;
-  constructor(private http: HttpClient) {
+  constructor() {
     this.rightclickdata =
       [{ "text": "Add New", "icon": "fa fa-plus", "disabled": true }, { "text": "Edit", "icon": "", "seperator": true }
         , { "text": "Send data in email", "icon": "" }];
-    this.getHtmlAndTypeScriptCode();
+   
   }
-  getDta() {
-    this.asyncFlag = true;
-    setTimeout(() => {
-      this.asyncFlag = false;
-    }, 3000);
-  }
-  //TO LOAD HTML AND TYPESCRIPT CODE
-  getHtmlAndTypeScriptCode() {
-    let responseHtml: any;
-    let responseTs: any;
-
-    //HTML FILE
-    this.http.get('assets/data/code/layout/panel/panel.html', { responseType: 'text' }).subscribe(data => {
-      responseHtml = data;
-    }, error => {
-    }, () => {
-      this.htmlCode = responseHtml;
-    });
-
-    //TS FILE
-    this.http.get('assets/data/code/layout/panel/panel.text', { responseType: 'text' }).subscribe(data => {
-      responseTs = data;
-    }, error => {
-    }, () => {
-      this.typeScriptCode = responseTs;
-    });
-
-  }
-
+ 
+  
   //THIS METHOD USED FOR COPY THE HTML & TYPESCRIPT CODE
   onCopyClick() {
     if (this.copyMsgArray.length >= 1) {
