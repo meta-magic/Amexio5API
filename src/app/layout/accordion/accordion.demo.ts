6420e9edfc71a3b2ccca98d8f57e97a6971543a6
kedar8 <kedar.kokil@metamagic.in>
2019-02-01 19:10:42 +0530

    accordion : code  updated  & json updated.

--- src/app/layout/accordion/accordion.demo.ts
+++ src/app/layout/accordion/accordion.demo.ts
@@ -2,48 +2,14 @@
  * Created by pratik on 16/1/18.
  */
 import {Component, OnInit, ViewEncapsulation} from '@angular/core';
-import {HttpClient} from "@angular/common/http";
-
 @Component({
   selector: 'accordion-demo',
 templateUrl: './accordion.demo.html',
 })
 
 export class AccordionDemoComponent{
-  htmlCode: string;
-  typeScriptCode: string;
   copyMsgArray: any[];
-  asyncFlag : boolean;
-  constructor(private http: HttpClient) {
-    this.getHtmlAndTypeScriptCode();
-  }
-  getDta() {
-    this.asyncFlag = true;
-    setTimeout(() => {
-      this.asyncFlag = false;
-    }, 3000);
-  }
-  //TO LOAD HTML AND TYPESCRIPT CODE
-  getHtmlAndTypeScriptCode() {
-    let responseHtml: any;
-    let responseTs: any;
-
-    //HTML FILE
-    this.http.get('assets/data/code/layout/accordion/accordion.html',{responseType: 'text'}).subscribe(data => {
-      responseHtml = data;
-    }, error => {
-    }, () => {
-      this.htmlCode = responseHtml;
-    });
-
-    //TS FILE
-    this.http.get('assets/data/code/layout/accordion/accordion.text',{responseType: 'text'}).subscribe(data => {
-      responseTs = data;
-    }, error => {
-    }, () => {
-      this.typeScriptCode = responseTs;
-    });
-
+  constructor() {
   }
 
   //THIS METHOD USED FOR COPY THE HTML & TYPESCRIPT CODE
