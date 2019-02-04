f44a6159244bb2c51278b2d86f1ff55d68908fba
datsgawas <dattaram.gawas@metamagic.in>
2019-02-04 20:23:58 +0530

    dashboard html updated.

--- src/app/dashboard/datapoints/datapoint.demo.component.ts
+++ src/app/dashboard/datapoints/datapoint.demo.component.ts
@@ -2,56 +2,16 @@
  * Created by sagar on 11/1/18.
  */
 import {Component, OnInit} from '@angular/core';
-import {HttpClient} from "@angular/common/http";
 @Component({
-  selector: 'datapoint', 
+  selector: 'datapoint',
   templateUrl: './datapoint.demo.component.html',
 })
 export class DataPointComponent implements OnInit {
   gaugeChartData: any;
-  htmlCode: string;
-  typeScriptCode: string;
-  dataSourceCode: string;
-  copyMsgArray: any[];
 
-  constructor(private http: HttpClient) {
+  constructor() {
     this.gaugeChartData = [['Label', 'Value'], ['Memory', 80], ['CPU', 55], ['Network', 68]];
-    this.getHtmlAndTypeScriptCode();
   }
-
-  //TO LOAD HTML AND TYPESCRIPT CODE
-  getHtmlAndTypeScriptCode() {
-    let responseHtml: any;
-    let responseTs: any;
-
-    //HTML FILE
-    this.http.get('assets/data/code/dashboard/datapoint/datapoint.html',{responseType: 'text'}).subscribe(data => {
-      responseHtml = data;
-    }, error => {
-    }, () => {
-      this.htmlCode = responseHtml;
-    });
-
-    //TS FILE
-    this.http.get('assets/data/code/dashboard/datapoint/datapoint.text',{responseType: 'text'}).subscribe(data => {
-      responseTs = data;
-    }, error => {
-    }, () => {
-      this.typeScriptCode = responseTs;
-    });
-
-  }
-
-  //this code use copy to html code from tabpanel
-  onCopyClick() {
-    if (this.copyMsgArray.length >= 1) {
-      this.copyMsgArray = [];
-      this.copyMsgArray.push({'msg': 'Code Copied', 'type': 'info'});
-    } else {
-      this.copyMsgArray.push({'msg': 'Code Copied', 'type': 'info'});
-    }
-  }
-
   ngOnInit() {
   }
 
--- src/app/dashboard/gauge/gauge.demo.component.ts
+++ src/app/dashboard/gauge/gauge.demo.component.ts
@@ -1,67 +1,21 @@
 /**
  * Created by sagar on 11/1/18.
  */
-import {Component, OnInit} from '@angular/core';
-import {HttpClient} from "@angular/common/http";
+import {Component} from '@angular/core';
 @Component({
   selector: 'gauge-dashboard-demo',
- 
+
   templateUrl :'./gauge.demo.component.html',
 })
-export class GaugeDemoComponent implements OnInit {
+export class GaugeDemoComponent {
   gaugeChartData:any;
-  htmlCode: string;
-  typeScriptCode: string;
-  dataSourceCode:string;
-  copyMsgArray: any[];
-  constructor(private http: HttpClient) {
+  constructor() {
     this.gaugeChartData=[
       ['Label', 'Value'],
       ['Memory', 80],
       ['CPU', 55],
       ['Network', 68]
     ];
-    this.getHtmlAndTypeScriptCode();
-  }
-  //TO LOAD HTML AND TYPESCRIPT CODE
-  getHtmlAndTypeScriptCode() {
-    let responseHtml: any;
-    let responseTs:any;
-
-    //HTML FILE
-    this.http.get('assets/data/code/dashboard/gauge/chart.html',{responseType: 'text'}).subscribe(data => {
-      responseHtml = data;
-    }, error => {
-    }, () => {
-      this.htmlCode = responseHtml;
-    });
-
-    //TS FILE
-    this.http.get('assets/data/code/dashboard/gauge/chart.text',{responseType: 'text'}).subscribe(data => {
-      responseTs = data;
-    }, error => {
-    }, () => {
-      this.typeScriptCode = responseTs;
-    });
-    //DataSource FILE
-    this.http.get('assets/data/code/dashboard/gauge/datasource.json',{responseType: 'text'}).subscribe(data => {
-      responseTs = data;
-    }, error => {
-    }, () => {
-      this.dataSourceCode = responseTs;
-    });
-  }
-
-  //this code use copy to html code from tabpanel
-  onCopyClick(){
-    if(this.copyMsgArray.length>=1){
-      this.copyMsgArray=[];
-      this.copyMsgArray.push({'msg':'Code Copied', 'type' : 'info'});
-    }else{
-      this.copyMsgArray.push({'msg':'Code Copied', 'type' : 'info'});
-    }
-  }
-  ngOnInit() {
   }
 
 }
