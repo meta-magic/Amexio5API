93395f0a525093109764de83d15499775d7ddfef
datsgawas <dattaram.gawas@metamagic.in>
2019-02-04 20:09:10 +0530

    media components html updated.

--- src/app/media/video/video.demo.ts
+++ src/app/media/video/video.demo.ts
@@ -3,53 +3,13 @@
  */
 
 import {Component} from '@angular/core';
-import {HttpClient} from "@angular/common/http";
 
 @Component({
   selector: 'video-demo',
   templateUrl: './video.demo.html',
 })
 export class VideoDemo {
-  htmlCode: string;
-  typeScriptCode: string;
-  copyMsgArray: any[];
-
-  constructor(private http: HttpClient) {
-    this.getHtmlAndTypeScriptCode();
-  }
-
-  //TO LOAD HTML AND TYPESCRIPT CODE
-  getHtmlAndTypeScriptCode() {
-    let responseHtml: any;
-    let responseTs: any;
-
-    //HTML FILE
-    this.http.get('assets/data/code/media/video/media.html',{responseType: 'text'}).subscribe(data => {
-      responseHtml = data;
-    }, error => {
-    }, () => {
-      this.htmlCode = responseHtml;
-    });
-
-    //TS FILE
-    this.http.get('assets/data/code/media/video/media.text',{responseType: 'text'}).subscribe(data => {
-      responseTs = data;
-    }, error => {
-    }, () => {
-      this.typeScriptCode = responseTs;
-    });
-
-  }
-
-  //THIS METHOD USED FOR COPY THE HTML & TYPESCRIPT CODE
-  onCopyClick() {
-    if (this.copyMsgArray.length >= 1) {
-      this.copyMsgArray = [];
-      this.copyMsgArray.push({'msg': 'Code Copied', 'type': 'info'});
-    } else {
-      this.copyMsgArray.push({'msg': 'Code Copied', 'type': 'info'});
-    }
-  }
+  constructor() {}
 }
 
 
