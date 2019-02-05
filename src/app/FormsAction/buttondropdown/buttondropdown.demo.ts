f17e541cd48ddf5d7388c42d10d3ca50ec0b3c20
kedar8 <kedar.kokil@metamagic.in>
2019-02-05 16:21:24 +0530

    forms Action: code updated.

--- src/app/FormsAction/buttondropdown/buttondropdown.demo.ts
+++ src/app/FormsAction/buttondropdown/buttondropdown.demo.ts
@@ -4,10 +4,30 @@
 
 import {Component} from '@angular/core';
 
+import { ComponentDataStructure } from '../../apimetadata/models/component.structure';
+
+
 @Component({
   selector: 'buttondropdown-demo',
   templateUrl: './buttondropdown.demo.html',
+  
 })
 export class ButtonDropDownDemo {
-  constructor() {}
+  
+  customSourceData: ComponentDataStructure;
+  constructor() {
+    this.customSourceData = new ComponentDataStructure();
+
+  }
+  ngOnInit(): void {
+    this.createCustomSourceData();
+  }
+  createCustomSourceData() {
+    this.customSourceData.title = 'Button Dropdown';
+    this.customSourceData.description = 'A button component with various modes and configurations.';
+    this.customSourceData.sourceMetadata.htmlUrl = 'forms/buttondropdown/form.html';
+    this.customSourceData.sourceMetadata.tsUrl = 'forms/buttondropdown/form.text';
+    this.customSourceData.liveMetadata.stackblitzUrl = 'https://stackblitz.com/edit/amexio-v4-buttondropdown?embed=1&file=app/forms/buttondropdown/buttondropdown.demo.html&view=editor';
+  }
+
 }
