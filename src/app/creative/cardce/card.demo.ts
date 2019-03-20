804d86b4ff657f814ef7d9decf808bbe94e0ba36
kedar8 <kedar.kokil@metamagic.in>
2019-03-20 13:48:27 +0530

    stackblitzUrl: updated.

--- src/app/creative/cardce/card.demo.ts
+++ src/app/creative/cardce/card.demo.ts
@@ -10,84 +10,51 @@ import {HttpClient} from '@angular/common/http';
   selector: 'card-ce-demo', templateUrl: './card.demo.html'
 })
 export class CreativeCardDemo {
-  htmlCode: string;
-  typeScriptCode: string;
-  copyMsgArray: any[];
-  contactList: any = [];
-  constructor(private http: HttpClient) {
-      this.getHtmlAndTypeScriptCode();
-      this.contactList = [
-        {
-          'name': 'Ketan Gote',
-          'description': 'UI/UX, Microservice, Kubernetes, Docker ',
-          'imagepath': 'assets/images/profile/ketan.jpg',
-          'bgcolor': 'linear-gradient(40deg,#12c2e9, #ec38bc)'
+    
+    contactList: any = [];
+    constructor(private http: HttpClient) {
+        
+        this.contactList = [
+          {
+            'name': 'Ketan Gote',
+            'description': 'UI/UX, Microservice, Kubernetes, Docker ',
+            'imagepath': 'assets/images/profile/ketan.jpg',
+            'bgcolor': 'linear-gradient(40deg,#12c2e9, #ec38bc)'
+          },
+          {
+          'name': 'Sagar Jadhav',
+          'description': 'Angular, Java, Microservices',
+          'imagepath': 'assets/images/profile/sagar.jpeg',
+          'bgcolor': 'linear-gradient(40deg,#141E30,#243B55)'
         },
         {
-        'name': 'Sagar Jadhav',
-        'description': 'Angular, Java, Microservices',
-        'imagepath': 'assets/images/profile/sagar.jpeg',
-        'bgcolor': 'linear-gradient(40deg,#141E30,#243B55)'
-      },
-      {
-        'name': 'Dattaram Gawas',
-        'description': 'UI / UX Desinger',
-        'imagepath': 'assets/images/profile/dats.jpg',
-        'bgcolor': 'linear-gradient(40deg,#a8ff78, #78ffd6)'
-      },
-      {
-        'name': 'Rashmi Thakker',
-        'description': 'UI Developer',
-        'imagepath': 'assets/images/profile/rashmi.jpg',
-        'bgcolor': 'linear-gradient(40deg,#457fca, #5691c8)'
-      },
-      {
-        'name': 'Ashwini Agre',
-        'description': 'UI Developer',
-        'imagepath': 'assets/images/profile/ashwini.jpg',
-        'bgcolor': 'linear-gradient(40deg,#bc4e9c, #f80759)'
-      },
-      {
-        'name': 'Deepali Arvind',
-        'description': 'Security Analyst',
-        'imagepath': 'assets/images/profile/dipali.jpg',
-        'bgcolor': 'linear-gradient(40deg,#40E0D0, #FF0080)'
-      }
-      ];
-  }
-
-  //TO LOAD HTML AND TYPESCRIPT CODE
-  getHtmlAndTypeScriptCode() {
-    let responseHtml: any;
-    let responseTs: any;
-
-    //HTML FILE
-    this.http.get('assets/data/code/creative/cardce/creative.html',{responseType: 'text'}).subscribe(data => {
-      responseHtml = data;
-    }, error => {
-    }, () => {
-      this.htmlCode = responseHtml;
-    });
-
-    //TS FILE
-    this.http.get('assets/data/code/creative/cardce/creative.text',{responseType: 'text'}).subscribe(data => {
-      responseTs = data;
-    }, error => {
-    }, () => {
-      this.typeScriptCode = responseTs;
-    });
-
-  }
-
-  //THIS METHOD USED FOR COPY THE HTML & TYPESCRIPT CODE
-  onCopyClick() {
-    if (this.copyMsgArray.length >= 1) {
-      this.copyMsgArray = [];
-      this.copyMsgArray.push({'msg': 'Code Copied', 'type': 'info'});
-    } else {
-      this.copyMsgArray.push({'msg': 'Code Copied', 'type': 'info'});
+          'name': 'Dattaram Gawas',
+          'description': 'UI / UX Desinger',
+          'imagepath': 'assets/images/profile/dats.jpg',
+          'bgcolor': 'linear-gradient(40deg,#a8ff78, #78ffd6)'
+        },
+        {
+          'name': 'Rashmi Thakker',
+          'description': 'UI Developer',
+          'imagepath': 'assets/images/profile/rashmi.jpg',
+          'bgcolor': 'linear-gradient(40deg,#457fca, #5691c8)'
+        },
+        {
+          'name': 'Ashwini Agre',
+          'description': 'UI Developer',
+          'imagepath': 'assets/images/profile/ashwini.jpg',
+          'bgcolor': 'linear-gradient(40deg,#bc4e9c, #f80759)'
+        },
+        {
+          'name': 'Deepali Arvind',
+          'description': 'Security Analyst',
+          'imagepath': 'assets/images/profile/dipali.jpg',
+          'bgcolor': 'linear-gradient(40deg,#40E0D0, #FF0080)'
+        }
+        ];
     }
+
+    
   }
-}
 
 
--- src/app/creative/homepage/homepage.demo.ts
+++ src/app/creative/homepage/homepage.demo.ts
@@ -17,49 +17,12 @@ import {HttpClient} from '@angular/common/http';
   ],
 })
 export class HomePageDemo {
-  htmlCode: string;
-  typeScriptCode: string;
-  copyMsgArray: any[];
-  contactList: any = [];
+ 
   DEMO_URL = 'https://meta-magic.github.io/amexio-ce-demo/#/home/dashboard?type=';
   constructor(private http: HttpClient) {
-      this.getHtmlAndTypeScriptCode();
   }
-
   onHomePageClick(type: string) {
     window.open(this.DEMO_URL + type, '_blank');
-  }
-  // TO LOAD HTML AND TYPESCRIPT CODE
-  getHtmlAndTypeScriptCode() {
-    let responseHtml: any;
-    let responseTs: any;
-
-    // HTML FILE
-    this.http.get('assets/data/code/creative/homepage/creative.html', {responseType: 'text'}).subscribe(data => {
-      responseHtml = data;
-    }, error => {
-    }, () => {
-      this.htmlCode = responseHtml;
-    });
-
-    // TS FILE
-    this.http.get('assets/data/code/creative/homepage/creative.text', {responseType: 'text'}).subscribe(data => {
-      responseTs = data;
-    }, error => {
-    }, () => {
-      this.typeScriptCode = responseTs;
-    });
-
-  }
-
-  // THIS METHOD USED FOR COPY THE HTML & TYPESCRIPT CODE
-  onCopyClick() {
-    if (this.copyMsgArray.length >= 1) {
-      this.copyMsgArray = [];
-      this.copyMsgArray.push({'msg': 'Code Copied', 'type': 'info'});
-    } else {
-      this.copyMsgArray.push({'msg': 'Code Copied', 'type': 'info'});
-    }
   }
 }
 
