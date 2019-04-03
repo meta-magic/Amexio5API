209f77cafa1c578062e4bb514215da9a7538abb4
kedar8 <kedar.kokil@metamagic.in>
2019-04-03 17:13:40 +0530

    creative card: card header  new attribute is added apply-theme-color.

--- src/app/creative/creative.module.ts
+++ src/app/creative/creative.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from "@angular/core";
 import { AmexioWidgetModule } from "amexio-ng-extensions";
-import { FormsModule,ReactiveFormsModule } from "@angular/forms";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 import { CommonModule } from "@angular/common";
 import { HttpClientModule } from "@angular/common/http";
 import { SharedModule } from "../shared.module";
@@ -11,14 +11,14 @@ import { RouterModule } from "@angular/router";
 import { CreativeCardDemo } from "./cardce/card.demo";
 import { CreativeWindowDemo } from "./windowce/window.demo";
 import { CreativeFormDemo } from "./formce/form.demo";
-import {CreativeCircularProgressBarDemo} from './progressce/progressce.component';
+import { CreativeCircularProgressBarDemo } from './progressce/progressce.component';
 import { HomePageDemo } from './homepage/homepage.demo';
 
 /**
  * Created by kedar on 6/2/19.
  */
 @NgModule({
-    declarations: [CreativeCardDemo,CreativeWindowDemo,CreativeFormDemo, CreativeCircularProgressBarDemo,HomePageDemo],
+    declarations: [CreativeCardDemo, CreativeWindowDemo, CreativeFormDemo, CreativeCircularProgressBarDemo, HomePageDemo],
     imports: [
         CommonModule,
         FormsModule,
