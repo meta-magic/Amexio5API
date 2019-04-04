3024d9cec512214bb81259df672c98b79133bde8
kedar8 <kedar.kokil@metamagic.in>
2019-04-04 20:20:12 +0530

    floating-group-button: size attribute added. and demo updated.

--- src/app/FormsAction/floatinggroup/floatinggroupbutton.demo.ts
+++ src/app/FormsAction/floatinggroup/floatinggroupbutton.demo.ts
@@ -2,26 +2,38 @@
  * Created by sagar on 9/1/18.
  */
 
-import {Component, OnInit} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ComponentDataStructure } from '../../apimetadata/models/component.structure';
 
 @Component({
   selector: 'floating-group-button-demo',
-  templateUrl :'./floatinggroupbutton.demo.html',
+  templateUrl: './floatinggroupbutton.demo.html',
 })
 export class FloatingGroupButtonDemo implements OnInit {
-  asyncFlag : boolean;
-  floatingbuttongroup : any[];
+  asyncFlag: boolean;
+  floatingbuttongroupOne: any[];
+  floatingbuttongroupTwo: any[];
+  floatingbuttongroupThree: any[];
   customSourceData: ComponentDataStructure;
   constructor() {
     this.customSourceData = new ComponentDataStructure();
 
 
-      this.floatingbuttongroup = [
-        {'label':'Facebook', 'icon':'fa fa-facebook','type':'yellow'},
-        {'label':'Twitter', 'icon':'fa fa-twitter','type':'green'},
-        {'label':'Google', 'icon':'fa fa-google-plus','type':'theme-color'}
-      ];
+    this.floatingbuttongroupOne = [
+      { 'label': 'Facebook', 'icon': 'fa fa-facebook', 'type': 'yellow' },
+      { 'label': 'Twitter', 'icon': 'fa fa-twitter', 'type': 'green' },
+      { 'label': 'Google', 'icon': 'fa fa-google-plus', 'type': 'theme-color' }
+    ];
+    this.floatingbuttongroupTwo = [
+      { 'label': 'Facebook', 'icon': 'fa fa-facebook', 'type': 'yellow' },
+      { 'label': 'Twitter', 'icon': 'fa fa-twitter', 'type': 'green' },
+      { 'label': 'Google', 'icon': 'fa fa-google-plus', 'type': 'theme-color' }
+    ];
+    this.floatingbuttongroupThree = [
+      { 'label': 'Facebook', 'icon': 'fa fa-facebook', 'type': 'yellow' },
+      { 'label': 'Twitter', 'icon': 'fa fa-twitter', 'type': 'green' },
+      { 'label': 'Google', 'icon': 'fa fa-google-plus', 'type': 'theme-color' }
+    ];
   }
 
   ngOnInit(): void {
@@ -35,7 +47,7 @@ export class FloatingGroupButtonDemo implements OnInit {
     this.customSourceData.liveMetadata.stackblitzUrl = 'https://stackblitz.com/edit/amexio-v41-floatingbuttongroup?embed=1&file=app/forms/floatingbuttongroup/floatingbuttongroup.demo.html&view=editor';
   }
 
-  onFloatingClick(event:any){
+  onFloatingClick(event: any) {
   }
 
 }
