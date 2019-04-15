b1f7bafa5724f202af41918bf68f75c7d208d11e
rashmithakkar <rashmi.thakkar@metamagic.in>
2019-04-15 11:25:34 +0530

    accessibility: updated

--- src/app/accessibility/accessibility.demo.ts
+++ src/app/accessibility/accessibility.demo.ts
@@ -5,6 +5,7 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from "@angular/common/http";
 import { Router } from "@angular/router";
+import { AmexioGridLayoutService, GridConfig, GridConstants} from 'amexio-ng-extensions';
 
 @Component({
     selector: 'accesibility-page',
@@ -17,15 +18,27 @@ export class AccessibilityDemo implements OnInit {
     userDefineColors: any;
     sourceData: any;
     copyMsgArray: any[];
+    gridDesktop: GridConfig;
 
-    constructor(private http: HttpClient,public router: Router) {
 
+    constructor(private http: HttpClient,public router: Router, private _gridlayoutService : AmexioGridLayoutService) {
+        this.createLayouts();
+    
+        // Create the Layouts
+        this._gridlayoutService.createLayout(this.gridDesktop);
     }
     ngOnInit() {
 
     }
     onRowSelect(data:any){
         this.router.navigate([data.link]);
+    }
+
+    createLayouts() {
+        this.gridDesktop = new GridConfig('LayoutSample1', GridConstants.Desktop)
+        .addlayout(["gridtable1", "gridtable2", "gridtable7"])
+        .addlayout(["gridtable3", "gridtable4", "gridtable7"])
+        .addlayout(["gridtable5", "gridtable6", "gridtable7"]);
     }
 }
 
