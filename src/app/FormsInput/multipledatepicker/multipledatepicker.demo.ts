0f2cbc08a8ebd56f7ff0e73afdd40cbbd6bf6109
Manisha1205 <manisha.boddu@metamagic.in>
2019-04-23 20:16:31 +0530

    multidatepicker updated

---
+++ src/app/FormsInput/multipledatepicker/multipledatepicker.demo.ts
@@ -0,0 +1,38 @@
+/**
+ * Created by Manisha on 23/4/19.
+ */
+
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'multiple-date-picker-demo',
+  templateUrl: './multipledatepicker.demo.html',
+})
+export class MultipleDateTimePickerDemo {
+  copyMsgArray: any[];
+  currentDate: any;
+   
+  time: any;
+  disabledDate: any[];
+  date = new Date("March 21, 2018 01:15:00");
+
+  onChangeTime(data: any) {
+    this.time = data;
+  }
+
+  constructor() {
+   
+  }
+
+  //THIS METHOD USED FOR COPY THE HTML & TYPESCRIPT CODE
+  onCopyClick() {
+    if (this.copyMsgArray.length >= 1) {
+      this.copyMsgArray = [];
+      this.copyMsgArray.push({ 'msg': 'Code Copied', 'type': 'info' });
+    } else {
+      this.copyMsgArray.push({ 'msg': 'Code Copied', 'type': 'info' });
+    }
+  }
+}
+
+
