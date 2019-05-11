87b5872a61e0de23c0157145466242e214a6347d
rashmithakkar <rashmi.thakkar@metamagic.in>
2019-05-11 09:00:19 +0530

    casrd: updated

--- src/app/directive/colorpalette/color-palette.ts
+++ src/app/directive/colorpalette/color-palette.ts
@@ -4,6 +4,7 @@
 
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from "@angular/common/http";
+import { Router } from "@angular/router";
 
 @Component({
         selector: 'color-palette-demo',
@@ -19,7 +20,7 @@ export class ColorPaletteDemo implements OnInit {
         copyMsgArray: any[];
         inputData: any;
         componentData: any;
-        constructor(private http: HttpClient) {
+        constructor(private http: HttpClient, public router: Router) {
                 this.inputData =
                         [{
                                 input: 'color-palette',
@@ -35,31 +36,58 @@ export class ColorPaletteDemo implements OnInit {
                         this.componentData =
                         [{
                                 componentName: 'amexio-navbar',
-                                colorpalette: 'amexio-theme-color1'
+                                colorpalette: 'amexio-theme-color1',
+                                link: "navigation/navbar-demo"
+
                         }, {
                                 componentName: 'amexio-panel',
-                                colorpalette: 'amexio-theme-color2'
+                                colorpalette: 'amexio-theme-color2',
+                                link: "panes/panel-demo"
+
                         }, {
                                 componentName: 'amexio-accordion',
-                                colorpalette: 'amexio-theme-color3'
+                                colorpalette: 'amexio-theme-color3',
+                                link: "layout/accordion-demo"
+
                         },{
                                 componentName: 'amexio-card',
-                                colorpalette: 'amexio-theme-color4'
+                                colorpalette: 'amexio-theme-color4',
+                                link: "layout/card-form-demo"
+
                         },{
                                 componentName: 'amexio-grid',
-                                colorpalette: 'amexio-theme-color5'
+                                colorpalette: 'amexio-theme-color5',
+                                link: "data/simplegrid-demo"
                         },{
                                 componentName: 'amexio-window',
-                                colorpalette: 'amexio-theme-color6'
+                                colorpalette: 'amexio-theme-color6',
+                                link: "panes/window-demo"
+
                         },{
                                 componentName: 'amexio-box',
-                                colorpalette: 'amexio-theme-color6'
+                                colorpalette: 'amexio-theme-color6',
+                                link: "layout/box-demo"
+
                         },{
                                 componentName: 'amexio-dialogue',
-                                colorpalette: 'amexio-theme-color6'
+                                colorpalette: 'amexio-theme-color6',
+                                link: "panes/dialogue-demo"
+
                         },{
                                 componentName: 'amexio-tab',
-                                colorpalette: 'amexio-theme-color3'
+                                colorpalette: 'amexio-theme-color3',
+                                link: "panes/basic-tab-demo"
+
+                        },{
+                                componentName: 'amexio-ce-card',
+                                colorpalette: 'amexio-theme-color4',
+                                link: "/creative/card-ce-demo"
+
+                        },{
+                                componentName: 'amexio-ce-window',
+                                colorpalette: 'amexio-theme-colo6',
+                                link: "creative/window-ce-demo"
+
                         }
                         ]
 
@@ -68,6 +96,11 @@ export class ColorPaletteDemo implements OnInit {
         ngOnInit() {
 
         }
+
+        onRowSelect(data:any){
+                debugger;
+                this.router.navigate([data.link]);
+            }
 
 }
 
