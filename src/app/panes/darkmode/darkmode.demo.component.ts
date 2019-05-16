06b7c0b0586a22773c981849a96ba65113e2ff2e
rashmithakkar <rashmi.thakkar@metamagic.in>
2019-05-16 16:19:12 +0530

    darkmode: slider added

--- src/app/panes/darkmode/darkmode.demo.component.ts
+++ src/app/panes/darkmode/darkmode.demo.component.ts
@@ -11,7 +11,22 @@ import { Component } from '@angular/core';
 export class DarkmodeDemoComponent  {
  
   copyMsgArray: any[];
+  colorData: any;
   constructor() {
+    this.colorData = [{
+      bgColor:'#ffffff',
+      fgColor:'#000000'
+    },{
+      bgColor:'#c6ae71',
+      fgColor:'#182618'
+    },{
+      bgColor:'#937427',
+      fgColor:'#c7e0c7'
+    },{
+      bgColor:'#4c3e1d',
+      fgColor:'#c7e0c7'
+    },
+  ]
   }
  
   //THIS METHOD USED FOR COPY THE HTML & TYPESCRIPT CODE
