62c11e4966b6d69fd6ed11eb8f30f172ce59c364
Manisha1205 <manisha.boddu@metamagic.in>
2019-05-21 20:13:23 +0530

    sliding panel added

--- src/app/panes/slidingpanel/animatedslidingpane.demo.ts
+++ src/app/panes/slidingpanel/animatedslidingpane.demo.ts
@@ -3,6 +3,8 @@
  */
 
 import { Component } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {PropertyGridModel} from 'amexio-ng-extensions';
 
 @Component({
   selector: 'animatedsliding-demo',
@@ -10,11 +12,32 @@ import { Component } from '@angular/core';
 })
 export class SlidingPanelDemo {
   copyMsgArray: any[];
-   
-  constructor() {
+  profileKeyValueData: any[] = []
+
+  constructor(private _httpClient: HttpClient) {
    
   }
+  
+  ngOnInit() {
+    this.getData();
+  }
 
+  getData() {
+    this._httpClient.get('assets/data/componentdata/slidingpanelpropertygrid.json').subscribe(
+      (res: any) => {
+         this.createProfileGrid(res.profileDetails);
+      });
+  }
+
+  createProfileGrid(profile: any) {
+    this.profileKeyValueData = [];
+     
+    this.profileKeyValueData.push(new PropertyGridModel('Email id',"john.doe@gmail.com", '', false));
+    this.profileKeyValueData.push(new PropertyGridModel('Birth date', '28-Feb-1950 ', '', false));
+    this.profileKeyValueData.push(new PropertyGridModel('Mobile Number', '+91-1234567890', '', false));
+    this.profileKeyValueData.push(new PropertyGridModel('Country', 'INDIA', '', false));
+
+  }
   //THIS METHOD USED FOR COPY THE HTML & TYPESCRIPT CODE
   onCopyClick() {
     if (this.copyMsgArray.length >= 1) {
