21dfbd2e2d7dd4d7fb163a14918b7c3e412639cd
kedar8 <kedar.kokil@metamagic.in>
2019-05-22 14:14:41 +0530

    bug: fixed.

--- src/app/FormsInput/socialmedialogin/googlelogin/googlelogin.demo.ts
+++ src/app/FormsInput/socialmedialogin/googlelogin/googlelogin.demo.ts
@@ -9,39 +9,17 @@ import {HttpClient} from '@angular/common/http';
   templateUrl: './googlelogin.demo.html',
 })
 export class GoogleLoginDemo {
-  htmlCode: string;
-  typeScriptCode: string;
+  
   copyMsgArray: any[];
   googleLoginData: any;
   constructor(private http: HttpClient) {
-    this.getHtmlAndTypeScriptCode();
+ 
   }
 
   onLoginClick(data: any) {
+    console.log('aaaaaaa',data);
+    
     this.googleLoginData = data;
-  }
-
-  // TO LOAD HTML AND TYPESCRIPT CODE
-  getHtmlAndTypeScriptCode() {
-    let responseHtml: any;
-    let responseTs: any;
-
-    // HTML FILE
-    this.http.get('assets/data/code/forms/googlelogin/form.html', {responseType: 'text'}).subscribe(data => {
-      responseHtml = data;
-    }, error => {
-    }, () => {
-      this.htmlCode = responseHtml;
-    });
-
-    // TS FILE
-    this.http.get('assets/data/code/forms/googlelogin/form.text', {responseType: 'text'}).subscribe(data => {
-      responseTs = data;
-    }, error => {
-    }, () => {
-      this.typeScriptCode = responseTs;
-    });
-
   }
 
   // THIS METHOD USED FOR COPY THE HTML & TYPESCRIPT CODE
