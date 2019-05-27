9edc6802649de097b0171a76326c73fd951fc699
ketangote <ketan.gote@metamagic.in>
2019-05-27 20:24:30 +0530

    Changes: Updated Theme Demo

--- src/app/enterprise/themeswitcher/themeswitcher.component.ts
+++ src/app/enterprise/themeswitcher/themeswitcher.component.ts
@@ -6,14 +6,28 @@ import { Component, OnInit } from "@angular/core";
 })
 export class ThemeSwitcherDemo implements OnInit{
 
-    toggle = false;
+    toggle = true;
+    relative = true;
+
+    vpos = 'top';
+    hpos = 'left';
     colsize = 3;
-    colsizedata : any[];
-    constructor(){
-        this.colsizedata = [];
+
+    vposdata = [];
+    hposdata = [];
+    colsizedata = [];
+
+    constructor(){        
         this.colsizedata.push({"colsize":3});
         this.colsizedata.push({"colsize":2});
         this.colsizedata.push({"colsize":1});
+        
+        this.hposdata.push({'display':'Right','value':'right'});
+        this.hposdata.push({'display':'Left','value':'left'});
+        
+        this.vposdata.push({'display':'Top','value':'top'});
+        this.vposdata.push({'display':'Bottom','value':'bottom'});
+        
     }
 
     ngOnInit(){
@@ -24,7 +38,11 @@ export class ThemeSwitcherDemo implements OnInit{
         this.toggle = !this.toggle;
     }
 
-    onColSizeSelected(event:any){
-
+    onClose(event:any){
+        this.toggle = false;
+    }
+    showTS(event:any){
+        debugger;
+        this.toggle = true;
     }
 }
