013b5a52dbbf9dce8a4fd8c379b5b755d1a6db3c
ankita202 <ankita.jain@metamgic.in>
2019-05-31 20:21:20 +0530

    Ribbon:Demo updated

--- src/app/creative/ribbonce/ribbon.demo.ts
+++ src/app/creative/ribbonce/ribbon.demo.ts
@@ -2,7 +2,7 @@
  * Created by Ankita on 27/5/19.
  */
 
-import {Component, OnInit} from '@angular/core'
+import { Component, OnInit } from '@angular/core'
 import { HttpClient } from '@angular/common/http';
 
 import { ComponentDataStructure } from '../../apimetadata/models/component.structure';
@@ -11,18 +11,63 @@ import { Router } from "@angular/router";
   selector: 'ribbon-card-demo',
   templateUrl: './ribbon.demo.html'
 })
-export class RibbonCardDemo implements OnInit{
- 
+export class RibbonCardDemo implements OnInit {
+
   copyMsgArray: any[];
   flag: boolean;
-
+  cardData: any[] = [];
   customSourceData: ComponentDataStructure;
 
 
-  constructor(private http: HttpClient,public router: Router) {
+  constructor(private http: HttpClient, public router: Router) {
     this.customSourceData = new ComponentDataStructure();
 
     this.flag = true;
+    this.cardData = [
+
+      {
+        "cardheader": "Free Plan",
+        "path": "assets/images/logos/Amexio.webp",
+        "contact": "Contact",
+        "price": "Free",
+        "colorpalette":"",
+        "styletype": "",
+        "buttonlabel": "buy",
+        "popularity": ""
+      },
+      {
+        "cardheader": " Bronze Package",
+        "path": "assets/images/logos/Amexio.webp",
+        "contact": "Contact",
+        "price": "3000",
+        "colorpalette":"",
+        "styletype": "",
+        "buttonlabel": "buy",
+        "popularity": ""
+      },
+      {
+        "cardheader": "Silver Package",
+        "path": "assets/images/logos/Amexio.webp",
+        "contact": "Contact",
+        "price": "6000",
+        "colorpalette":"classic",
+        "styletype": "ribbon-style",
+        "buttonlabel": "buy",
+        "popularity": "Most Popular"
+      },
+      {
+        "cardheader": "Gold Package",
+        "path": "assets/images/logos/Amexio.webp",
+        "contact": "Contact",
+        "price": "8500",
+        "colorpalette":"",
+        "styletype": "",
+        "buttonlabel": "buy",
+        "popularity": ""
+      }
+
+    ]
+
   }
 
   ngOnInit(): void {
@@ -33,12 +78,12 @@ export class RibbonCardDemo implements OnInit{
   onCopyClick() {
     if (this.copyMsgArray.length >= 1) {
       this.copyMsgArray = [];
-      this.copyMsgArray.push({'msg': 'Code Copied', 'type': 'info'});
+      this.copyMsgArray.push({ 'msg': 'Code Copied', 'type': 'info' });
     } else {
-      this.copyMsgArray.push({'msg': 'Code Copied', 'type': 'info'});
+      this.copyMsgArray.push({ 'msg': 'Code Copied', 'type': 'info' });
     }
   }
-  
+
 }
 
 
