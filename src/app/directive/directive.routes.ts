9c24e34fd12f2a98cc9b3f737537085865c77d14
rashmithakkar <rashmi.thakkar@metamagic.in>
2019-06-06 19:07:02 +0530

    inputmask: demo added

--- src/app/directive/directive.routes.ts
+++ src/app/directive/directive.routes.ts
@@ -1,9 +1,14 @@
 import { Routes } from '@angular/router';
 import { ColorPaletteDemo } from './colorpalette/color-palette';
+import { InputMaskDemo } from './inputmask/input-mask.demo';
 
 export const DIRECTIVE_ROUTE: Routes = [
 
     {
         path: 'color-palette', component: ColorPaletteDemo,
+    },
+    {
+        path: 'input-mask', component: InputMaskDemo,
     }
+
 ]
