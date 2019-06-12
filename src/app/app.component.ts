ecd4c8377657eaf8c051d3890f8df8486fe0dac0
rashmithakkar <rashmi.thakkar@metamagic.in>
2019-06-12 17:52:48 +0530

    submenus : changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -262,18 +262,19 @@ export class AppComponent {
             "link": "https://eedemo.amexio.org/#/home/dashboardtwo"
           },
           {
-            "text": "Movie Portal",
-            "link": "https://cedemo.amexio.org/Virtual-Scroller/#/sc",
+            "text": "Creative Home Page",
+            "link": "https://cedemo.amexio.org/amexio-ce-demo-app/index.html#/ce-demo-app",
             "separator": true,
             "separatorHeader": "Creative Edition Apps"
           },
           {
-            "text": "TecMFlix",
-            "link": "https://cedemo.amexio.org/tecmflix/"
+            "text": "Movie Portal",
+            "link": "https://cedemo.amexio.org/Virtual-Scroller/#/sc"
+           
           },
           {
-            "text": "Creative Home Page",
-            "link": "https://cedemo.amexio.org/amexio-ce-demo-app/index.html#/ce-demo-app"
+            "text": "TecMFlix",
+            "link": "https://cedemo.amexio.org/tecmflix/"
           },
           {
             "text": "Components App",
