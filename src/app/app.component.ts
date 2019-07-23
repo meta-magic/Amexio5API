0e5a576eb6dfbda4adb099a61f95f80146ebac66
rashmithakkar <rashmi.thakkar@metamagic.in>
2019-07-23 19:17:59 +0530

    version: changed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -100,6 +100,10 @@ export class AppComponent {
               "separator": true,
               "submenus": [
                 {
+                  "text": "Version 5.15",
+                  "link": "../v5.15/index.html"
+                },
+                {
                   "text": "Version 5.14",
                   "link": "../v5.14/index.html"
                 },
