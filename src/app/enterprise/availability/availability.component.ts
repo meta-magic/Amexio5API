cb6d6870107387a119850cb8bef4d696d23224e6
rashmithakkar <rashmi.thakkar@metamagic.in>
2019-09-27 21:44:18 +0530

    availabilty: updated

--- src/app/enterprise/availability/availability.component.ts
+++ src/app/enterprise/availability/availability.component.ts
@@ -1,20 +1,93 @@
 /**
  * Created by kedar on 29/9/19.
  */
-import {Component, OnInit} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'ee-availability', 
+  selector: 'ee-availability',
   templateUrl: 'availability.component.html'
 })
 
 export class AvailabilityDemoComponent implements OnInit {
-  
 
+  tzonedata =
+    [{ timezone: 'New South Wales' }, { timezone: 'Greenwich Mean Time' },
+    { timezone: 'Central European' }, { timezone: 'Vladivostok Standard' },
+    { timezone: 'Omsk' }
+    ];
+
+  labelData = [
+    {
+      "label": "Inbound",
+      "colorcode": "red",
+      "available": [
+        {
+          "date": "01-Sep-2019",
+          "time": [
+            {
+              "starttime": 5,
+              "endtime": 6.30
+            },
+            // ,
+            // {
+            //   "starttime": 10,
+            //   "endtime": 1
+            // }
+          ]
+        },
+
+      ]
+    },
+    {
+      "label": "Outbound",
+      "colorcode": "blue",
+      "available": [
+        {
+          "date": "01-Sep-2019",
+          "time": [
+            {
+              "starttime": 6.30,
+              "endtime": 7
+            },
+            // {
+            //   "starttime": 11,
+            //   "endtime": 14
+            // }
+          ]
+        },
+        {
+          "date": "04-Sep-2019",
+          "time": [
+            {
+              "starttime": 9.30,
+              "endtime": 11
+            },
+            // {
+            //   "starttime": 11,
+            //   "endtime": 14
+            // }
+          ]
+        },
+        {
+          "date": "07-Sep-2019",
+          "time": [
+            {
+              "starttime": 11,
+              "endtime": 12
+            },
+            // {
+            //   "starttime": 11,
+            //   "endtime": 14
+            // }
+          ]
+        }
+      ]
+    },
+  ];
   constructor() {
-   }
+  }
   ngOnInit() {
   }
 
- 
+
 }
