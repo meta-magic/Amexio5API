ba5638024ac5ece4985330427eb2931a3ddfbd55
kedar8 <kedar.kokil@metamagic.in>
2019-10-07 09:27:30 +0530

    rich-editable: demo updated.

--- src/app/enterprise/rich-editable-textarea/rich-editable.demo.component.ts
+++ src/app/enterprise/rich-editable-textarea/rich-editable.demo.component.ts
@@ -4,18 +4,18 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
- selector: 'rich-editable-textarea',
- templateUrl :'./rich-editable.demo.component.html',
+  selector: 'rich-editable-textarea',
+  templateUrl: './rich-editable.demo.component.html',
 })
 
 export class RichEditableDemoComponent implements OnInit {
   HTMLCodeDisplay: any;
   constructor() {
-  
-  }
- ngOnInit() { }
 
- onSourceCodeDisplay(data: any) {
-   this.HTMLCodeDisplay = data;
- }
+  }
+  ngOnInit() { }
+
+  onSourceCodeDisplay(data: any) {
+    this.HTMLCodeDisplay = data;
+  }
 }
